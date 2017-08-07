b9e988dcad01e70dd31d8e36bd0477f97738678c
Jaifroid <egk10@cam.ac.uk>
2017-08-07 15:12:45 +0100

    Bin
    Former-commit-id: 78e27610f6697ca8b0082b370d1767f526ef6b94 [formerly 1df6719041b188ec11cfd59db0b9df4800c7826e]
    Former-commit-id: fc08084da410aa3ab8a87b5119d14ddf3a6419c8

--- bld/Release/PackageLayout/www/js/MathJax/config/config.js
+++ bld/Release/PackageLayout/www/js/MathJax/config/config.js
@@ -2,7 +2,14 @@ MathJax.Hub.Config({
   extensions: ["tex2jax.js"],
   jax: ["input/TeX", "output/HTML-CSS"],
   TeX: {
-	extensions: ["AMSmath.js", "AMSsymbols.js"]
+	extensions: ["AMSmath.js", "AMSsymbols.js", "noUndefined.js"],
+	noUndefined: {
+		attributes: {
+			mathcolor: "red",
+			mathbackground: "#FFEEEE",
+			mathsize: "90%"
+		}
+	}
   },
   "HTML-CSS": {
     availableFonts: ["TeX"],
