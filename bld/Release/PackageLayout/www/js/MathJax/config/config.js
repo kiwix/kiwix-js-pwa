8a221aafe0803f213ac2bd3769471510ecc32942
Jaifroid <egk10@cam.ac.uk>
2017-08-07 13:46:18 +0100

    Release 0.7.9.0
    This release includes MathJax support for displaying equations on Wikipedia pages. The display falls back to SVG images if the TEX string cannot be located.
    
    
    Former-commit-id: e54a69de7d687d7638d9dc10f8846616cd4f45de

---
+++ bld/Release/PackageLayout/www/js/MathJax/config/config.js
@@ -0,0 +1,16 @@
+MathJax.Hub.Config({
+  extensions: ["tex2jax.js"],
+  jax: ["input/TeX", "output/HTML-CSS"],
+  TeX: {
+	extensions: ["AMSmath.js", "AMSsymbols.js"]
+  },
+  "HTML-CSS": {
+    availableFonts: ["TeX"],
+    imageFont: null
+  },
+  MathMenu: {
+   showRenderer: false,
+   showFontMenu: false,
+   showLocale: false
+  }
+});
