22767423fad471969031e2e3486204a779f14b61
Jaifroid <egk10@cam.ac.uk>
2017-08-24 16:16:21 +0100

    Release 0.8.5 Beta

--- bld/Release/PackageLayout/www/js/init.js
+++ bld/Release/PackageLayout/www/js/init.js
@@ -22,6 +22,7 @@
  */
 'use strict';
 var params = {};
+params['version'] = "0.8.2 Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 var results = params['results'] || 15; //Number of search results to display
 params['cssSource'] = getCookie('cssSource') || "auto"; //Set default to "auto", "desktop" or "mobile"
 params['cssCache'] = getCookie('cssCache') || true; //Set default to true to use cached CSS, false to use Zim only
@@ -31,7 +32,32 @@ params['cssTheme'] = getCookie('cssTheme') || 'light'; //Set default to 'light' 
 params['cssUITheme'] = getCookie('cssUITheme') || 'light'; //Set default to 'light' or 'dark' to use respective themes for UI
 params['imageDisplay'] = getCookie('imageDisplay') || true; //Set default to display images from Zim
 params['imageDisplay'] = params['imageDisplay'] == "false" ? false : (params['imageDisplay'] == "true" ? true : params['imageDisplay']);
-params['useMathJax'] = params['useMathJax'] || true; //Set default to true to display math formulae with MathJax, false to use fallback SVG images
+params['useMathJax'] = params['useMathJax'] || true; //Set default to true to display math formulae with MathJax, false to use fallback SVG images only
+params['storedFile'] = getCookie('lastSelectedArchive') || "wikipedia_en_ray_charles_2015-06.zimaa"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['falFileToken'] = params['falFileToken'] || "zimfile";
+params['falFolderToken'] = params['falFolderToken'] || "zimfilestore";
+params['localStorage'] = params['localStorage'] || ""; //These will be set programmatically below
+params['pickedFile'] = params['pickeFile'] || "";
+params['pickedFolder'] = params['pickedFolder'] || "";
+
+if (params['storedFile'] && typeof Windows !== 'undefined' && typeof Windows.Storage !== 'undefined') { //UWP
+    //DEV change "archives" below if you wish to store local archives in a different location in the installation package
+    Windows.ApplicationModel.Package.current.installedLocation.getFolderAsync("archives").done(function (folder) {
+        if (folder) params['localStorage'] = folder;
+    });
+    var StorageApplicationPermissions = Windows.Storage.AccessCache.StorageApplicationPermissions;
+    if (StorageApplicationPermissions.futureAccessList.containsItem(params['falFileToken'])) {
+        StorageApplicationPermissions.futureAccessList.getFileAsync(params['falFileToken']).done(function (file) {
+            if (file) params['pickedFile'] = file;
+        });
+    }
+    if (StorageApplicationPermissions.futureAccessList.containsItem(params['falFolderToken'])) {
+        StorageApplicationPermissions.futureAccessList.getFolderAsync(params['falFolderToken']).done(function (folder) {
+            if (folder) params['pickedFolder'] = folder;
+        });
+    }
+}
+
 
 function getCookie(cname) {
     var name = cname + "=";
