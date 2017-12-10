7d7a8b01bda6ddf1d8d4af06c2aed80f7f78f24a
Jaifroid <egk10@cam.ac.uk>
2017-12-10 12:25:53 +0000

    Changes for Wikivoyage packaging
    Former-commit-id: 9340010757c1b82369d7fdf91fd0f6542e83e7a6
    Former-commit-id: 3dfc25f7fcf44fa10ba253f9c518c5550732b037

--- www/js/init.js
+++ www/js/init.js
@@ -22,8 +22,8 @@
  */
 'use strict';
 var params = {};
-params['version'] = "0.9.3 Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
-params['storedFile'] = getCookie('lastSelectedArchive') || "wikipedia_en_ray_charles_2015-06.zimaa"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['version'] = "0.9.3 Wikivoyage Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['storedFile'] = getCookie('lastSelectedArchive') || "wikivoyage_en_all_novid_2017-12.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['kiwixDownloadLink'] = "http://download.kiwix.org/zim/"; //Include final slash
 
 params['results'] = params['results'] || 15; //Number of search results to display
