c661925d0aaf256941bdc7df7b2c39f47bf35ce8
Jaifroid <egk10@cam.ac.uk>
2017-12-14 17:32:08 +0000

    Clarified usage instructions

--- www/js/init.js
+++ www/js/init.js
@@ -22,7 +22,7 @@
  */
 'use strict';
 var params = {};
-params['version'] = "0.9.3 Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "0.9.5 Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['storedFile'] = getCookie('lastSelectedArchive') || "wikipedia_en_ray_charles_2015-06.zimaa"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['fileVersion'] = "wikipedia_en_ray_charles_2015-06"; //Use generic name for actual file, and give version here
 params['kiwixDownloadLink'] = "http://download.kiwix.org/zim/"; //Include final slash
