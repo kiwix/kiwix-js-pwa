e0b2e43ad86f16d6bd76829c04b84bcaafb3484c
Jaifroid <egk10@cam.ac.uk>
2018-03-17 11:01:54 +0000

    Update Wikivoyage ZIM to 2018-03
    Former-commit-id: 82fb287b4f865a8e9508954dd970e3f0bdc37007
    Former-commit-id: a9799330d7138536ec464975fed77c60aef65333

--- www/js/init.js
+++ www/js/init.js
@@ -22,9 +22,9 @@
  */
 'use strict';
 var params = {};
-params['version'] = "0.9.9.3 Wikivoyage Beta-dev"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "0.9.9.4 Wikivoyage Beta-dev"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['packagedFile'] = "wikivoyage_en.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
-params['fileVersion'] = "wikivoyage_en_all_novid_2018-01 (6-Jan-2018)"; //Use generic name for actual file, and give version here
+params['fileVersion'] = "wikivoyage_en_all_novid_2018-03 (13-Mar-2018)"; //Use generic name for actual file, and give version here
 params['cachedStartPage'] = "Wikivoyage%3AOffline_reader_Expedition_Home_page.html" || false; //If you have cached the start page for quick start, give its URI here
 params['kiwixDownloadLink'] = "http://download.kiwix.org/zim/"; //Include final slash
 
