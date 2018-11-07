7380bb7470d22728127cfe77d5dc9a529c976f90
Jaifroid <egk10@cam.ac.uk>
2018-11-07 11:47:09 +0000

    Release Wikivoyage 0.9.9.81 beta
    Former-commit-id: a0869d06ad4a07753afda8fac9e228444fc4951a
    Former-commit-id: b5a89f0f9deee13b929b27afe512b8b5545472b2

--- www/js/init.js
+++ www/js/init.js
@@ -24,7 +24,7 @@
 
 // Parameters that define overall operation of app
 var params = {};
-params['version'] = "0.9.9.81 Wikivoyage Beta-dev"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "0.9.9.81 Wikivoyage Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['packagedFile'] = "wikivoyage_en.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['fileVersion'] = "wikivoyage_en_all_2018-10 (10-Oct-2018)"; //Use generic name for actual file, and give version here
 params['cachedStartPage'] = "Wikivoyage%3AOffline_reader_Expedition_Home_page.html" || false; //If you have cached the start page for quick start, give its URI here
