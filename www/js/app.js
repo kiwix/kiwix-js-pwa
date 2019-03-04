7f754858ba451763c597ceb906cb05a6b63522cf
Jaifroid <egk10@cam.ac.uk>
2019-03-04 11:54:13 +0000

    Deal with re-organized stylesheets in mwoffliner ZIMs
    # Conflicts:
    #	www/js/app.js
    
    
    Former-commit-id: ec8ad67c6ffc96e82e329a081bd3535cb0dca8d1

--- www/js/app.js
+++ www/js/app.js
@@ -2027,21 +2027,6 @@ define(['jquery', 'zimArchiveLoader', 'util', 'uiUtil', 'cookies', 'q', 'module'
          * @param {String} htmlArticle
          */
         function displayArticleInForm(dirEntry, htmlArticle) {
-            // Display Bootstrap warning alert if the landing page contains active content
-            if (!params.hideActiveContentWarning && params.isLandingPage) {
-                if (regexpActiveContent.test(htmlArticle)) uiUtil.displayActiveContentWarning();
-            }
-
-            // Replaces ZIM-style URLs of img, script, link and media tags with a data-kiwixurl to prevent 404 errors [kiwix-js #272 #376]
-            // This replacement also processes the URL to remove the path so that the URL is ready for subsequent jQuery functions
-            htmlArticle = htmlArticle.replace(regexpTagsWithZimUrl, '$1data-kiwixurl$2');
-            // Remove any empty media containers on page
-            htmlArticle = htmlArticle.replace(/(<(audio|video)\b(?:[^<]|<(?!\/\2))+<\/\2>)/ig, function (p0) {
-                return /(?:src|data-kiwixurl)\s*=\s*["']/.test(p0) ? p0 : '';
-            });
-
-            //@BUG WORKAROUND for Kiwix-JS-Windows #18
-            htmlArticle = htmlArticle.replace(/(<link\s+[^>]*?\bhref\s*=\s*["'])(s\/[\s\S]+(?!\.css))(["'])/gi, "$1../-/$2.css$3");
 
             //TESTING
             console.log("** HTML received **");
@@ -2071,6 +2056,28 @@ define(['jquery', 'zimArchiveLoader', 'util', 'uiUtil', 'cookies', 'q', 'module'
                     }
                 }
             }
+
+            // Display Bootstrap warning alert if the landing page contains active content
+            if (!params.hideActiveContentWarning && params.isLandingPage) {
+                if (regexpActiveContent.test(htmlArticle)) uiUtil.displayActiveContentWarning();
+            }
+
+            // Replaces ZIM-style URLs of img, script, link and media tags with a data-kiwixurl to prevent 404 errors [kiwix-js #272 #376]
+            // This replacement also processes the URL to remove the path so that the URL is ready for subsequent jQuery functions
+            htmlArticle = htmlArticle.replace(regexpTagsWithZimUrl, '$1data-kiwixurl$2');
+            // Remove any empty media containers on page
+            htmlArticle = htmlArticle.replace(/(<(audio|video)\b(?:[^<]|<(?!\/\2))+<\/\2>)/ig, function (p0) {
+                return /(?:src|data-kiwixurl)\s*=\s*["']/.test(p0) ? p0 : '';
+            });
+
+            // Remove any download alerts and active content hanging on from previous article
+            ['activeContent', 'downloadAlert'].forEach(function (id) {
+                var rmv = document.getElementById(id);
+                if (rmv) rmv.parentElement.removeChild(rmv);
+            });
+
+            //@BUG WORKAROUND for Kiwix-JS-Windows #18
+            htmlArticle = htmlArticle.replace(/(<link\s+[^>]*?\bhref\s*=\s*["'])(s\/[\s\S]+(?!\.css))(["'])/gi, "$1../-/$2.css$3");
 
             //Some documents (e.g. Ray Charles Index) can't be scrolled to the very end, as some content remains benath the footer
             //so add some whitespace at the end of the document
