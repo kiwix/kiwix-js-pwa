8d7b737fd6ac87a466e4603cad54bc24bcbfed84
Jaifroid <egk10@cam.ac.uk>
2019-03-06 16:00:45 +0000

    Add filter so that packaged apps only show appropriate ZIM files
    Former-commit-id: d93d4f5648e56b862024f69d7856c392aa9371ab
    Former-commit-id: 2a02b31b3fca3fa669ba25c4da66fda2dca8082b

--- www/js/init.js
+++ www/js/init.js
@@ -34,7 +34,7 @@ params['version'] = "0.9.9.88 WikiMed Beta-dev"; //DEV: do not set this dynamica
 params['packagedFile'] = "wikipedia_en_medicine.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['fileVersion'] = "wikipedia_en_medicine_novid_2018-10 (9-Oct-2018)"; //Use generic name for actual file, and give version here
 params['cachedStartPage'] = "Wikipedia%3AWikiProject_Medicine_Open_Textbook_of_Medicine2.html" || false; //If you have cached the start page for quick start, give its URI here
-params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/"; //Include final slash
+params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/wikipedia/"; //Include final slash
 
 params['results'] = params['results'] || 50; //Number of search results to display
 params['relativeFontSize'] = ~~(getCookie('relativeFontSize') || 100); //Sets the initial font size for articles (as a percentage) - user can adjust using zoom buttons
