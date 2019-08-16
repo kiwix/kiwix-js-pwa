5ddce50def2344c1cfa70975c319df544e7e6ac0
Jaifroid <egk10@cam.ac.uk>
2019-08-16 09:28:56 +0100

    Package latest Ray Charles for release
    Former-commit-id: 576c184f20279183df68aea7a532976341a74ebc

--- www/js/init.js
+++ www/js/init.js
@@ -32,9 +32,9 @@ var state = {};
 // Parameters that define overall operation of app
 var params = {};
 params['version'] = "0.9.9.93 Beta-dev"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
-params['packagedFile'] = false; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
-params['archivePath'] = ""; //The directory containing the packaged archive(s) (relative to app's root directory)  
-params['fileVersion'] = ""; //Use generic name for actual file, and give version here
+params['packagedFile'] = "wikipedia_en_ray_charles.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['archivePath'] = "archive"; //The directory containing the packaged archive(s) (relative to app's root directory)  
+params['fileVersion'] = "wikipedia_en_ray_charles_maxi_2019-08.zim (14-Aug-2019)"; //Use generic name for actual file, and give version here
 params['cachedStartPage'] = false; //If you have cached the start page for quick start, give its URI here
 params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/"; //Include final slash
 
