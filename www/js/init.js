ea33ada30307a45c34df0381bfbae9b56b9ad138
Jaifroid <egk10@cam.ac.uk>
2019-10-30 18:40:44 +0000

    Release 0.9.9.95 Beta
    Former-commit-id: a35adcbd58a1c018ffaeee19508a03cfc8cee896 [formerly 69105e6eedcad9305fcbbeeb7aa50474e35569ea]
    Former-commit-id: 422744b9ea93659134de03b27196ae438b03608f

--- www/js/init.js
+++ www/js/init.js
@@ -31,7 +31,7 @@ window.onerror = function (msg, url) {
 var state = {};
 // Parameters that define overall operation of app
 var params = {};
-params['version'] = "0.9.9.95 Beta-dev"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "0.9.9.95 Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['packagedFile'] = "wikipedia_en_ray_charles.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archive"; //The directory containing the packaged archive(s) (relative to app's root directory)  
 params['fileVersion'] = "wikipedia_en_ray_charles_maxi_2019-08.zim (14-Aug-2019)"; //Use generic name for actual file, and give version here
