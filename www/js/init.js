111c36897f649901513015e2ff03a522d4af73d0
Jaifroid <egk10@cam.ac.uk>
2020-01-01 21:03:58 +0000

    Provide option to set max search size
    Former-commit-id: b15757712ed086bfc892a07c540839eae8d3c502
    Former-commit-id: 6f7183d2a7abb4e4e47f363870a409c87b46642e

--- www/js/init.js
+++ www/js/init.js
@@ -38,7 +38,7 @@ params['cachedStartPage'] = "Main_Page" || false; //If you have cached the start
 params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/wikivoyage/"; //Include final slash
 
 params['cookieSupport'] = checkCookies();
-params['results'] = 20; //Number of search results to display
+params['maxResults'] = ~~(getCookie('maxResults') || 20); //Number of search results to display
 params['relativeFontSize'] = ~~(getCookie('relativeFontSize') || 100); //Sets the initial font size for articles (as a percentage) - user can adjust using zoom buttons
 params['relativeUIFontSize'] = ~~(getCookie('relativeUIFontSize') || 100); //Sets the initial font size for UI (as a percentage) - user can adjust using slider in Config
 params['cssSource'] = getCookie('cssSource') || "auto"; //Set default to "auto", "desktop" or "mobile"
@@ -118,6 +118,7 @@ document.getElementById('hideActiveContentWarningCheck').checked = params.hideAc
 document.getElementById('allowHTMLExtractionCheck').checked = params.allowHTMLExtraction;
 document.getElementById('alphaCharTxt').value = params.alphaChar;
 document.getElementById('omegaCharTxt').value = params.omegaChar;
+document.getElementById('maxResults').value = params.maxResults;
 
 var versionSpans = document.getElementsByClassName('version');
 for (var i = 0; i < versionSpans.length; i++) {
