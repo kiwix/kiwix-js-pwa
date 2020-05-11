d63268d93c02ace6126775a99a16af0a35b76a6d
Jaifroid <egk10@cam.ac.uk>
2020-05-11 22:51:59 +0100

    App packages for 0.9.9.9 WikiMed Beta
    Former-commit-id: a9dcb565a0527785dc380fcc5e31cd4752455232
    Former-commit-id: 43e79177b1b41094310a4a3927738da4353c409f

--- www/js/init.js
+++ www/js/init.js
@@ -43,7 +43,7 @@ var params = {};
  */
 var state = {};
 
-params['version'] = "0.9.9.99 WikiMed Beta-dev"; //DEV: Manually update this version when there is a new release: it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "0.9.9.99 WikiMed Beta"; //DEV: Manually update this version when there is a new release: it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['packagedFile'] = "wikipedia_en_medicine.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)  
 params['fileVersion'] = "wikipedia_en_medicine_maxi_2020-04 (25-Apr-2020)"; //Use generic name for actual file, and give version here
