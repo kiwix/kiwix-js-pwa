8108835f6ac8cba18ea513837b3be43d906285bc
Jaifroid <egk10@cam.ac.uk>
2020-08-27 17:31:46 +0100

    App packages for 0.9.9.992 Wikivoyage beta
    Former-commit-id: a1b1c56c019d1d5f1400fd96fc1be0ca89091725
    Former-commit-id: 12397cff07f5440cde3ed22556218f52ccce480b
    Former-commit-id: 395cf73a9c4fee08b5939999c6c1d1020ea7da1d

--- www/js/init.js
+++ www/js/init.js
@@ -49,10 +49,10 @@ var params = {};
  */
 var appstate = {};
 
-params['version'] = "0.9.9.991 Wikivoyage Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
-params['packagedFile'] = "wikivoyage_en.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['version'] = "0.9.9.992 Wikivoyage Beta"; //DEV: do not set this dynamically -- it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['packagedFile'] = "wikivoyage_en_all_maxi.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)
-params['fileVersion'] = "wikivoyage_en_all_maxi_2020-06 (3-Jun-2020)"; //Use generic name for actual file, and give version here
+params['fileVersion'] = "wikivoyage_en_all_maxi_2020-08 (4-Aug-2020)"; //Use generic name for actual file, and give version here
 params['cachedStartPage'] = "Main_Page"; //If you have cached the start page for quick start, give its URI here
 params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/wikivoyage/"; //Include final slash
 
