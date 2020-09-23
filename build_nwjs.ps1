e33b263ec0283339501dc9d823c968fd15aa6814
Jaifroid <egk10@cam.ac.uk>
2020-09-23 07:28:34 +0100

    Rebase nwjs-en-top on master
    Former-commit-id: eee0384ea5e4f388389d9517a0755864f67959e4
    Former-commit-id: 185fcff6692209ac53126d9c365dae4560bcb3f8
    Former-commit-id: ea636af4422d5e546bc0e517d6a284f18a1a880a

---
+++ build_nwjs.ps1
@@ -0,0 +1,19 @@
+$build = "win-x64"
+#$build = "win-ia32"
+$version = "0.46.3"
+#$version = "0.14.7"
+$appBuild = "0.9.9.991N"
+$ZIMbase = "wikipedia_en_100"
+$target = "bld\nwjs\$build-$version\kiwix_js_windows-$appBuild"
+$buildLocation = "node_modules\nwjs-builder-phoenix\caches\nwjs-v$version-$build.zip-extracted\nwjs-v$version-$build\"
+$fullTarget = "$target-$build"
+# $fullTarget
+$archiveFolder = "$fullTarget\archives"
+# Remove existing target
+rm $fullTarget\* -Recurse
+md $fullTarget
+# Copy latest binary x64
+cp $buildLocation\* $fullTarget -Recurse
+cp .\package.json, .\pwabuilder-sw.js, .\index.html, .\CHANGELOG.md, .\LICENSE, .\www $fullTarget -Recurse
+md $archiveFolder
+cp ".\archives\$ZIMbase*.*", .\archives\README.md $archiveFolder
