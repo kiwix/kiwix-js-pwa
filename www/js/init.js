920beb4f82e1d18c0fa3bf266b5dae004a6da390
Jaifroid <egk10@cam.ac.uk>
2020-10-20 16:27:14 +0100

    Rebase Kiwix JS WikiMed
    # Conflicts:
    #	CHANGELOG.md
    #	KiwixWebApp.jsproj
    #	KiwixWebApp.sln
    #	KiwixWebAppWikMed.sln
    #	KiwixWebAppWikiMed.jsproj
    #	KiwixWebAppWikivoyage.jsproj
    #	KiwixWebAppWikivoyage.sln
    #	Package.StoreAssociation.xml
    #	README.md
    #	images/BadgeLogo.scale-100.png
    #	images/BadgeLogo.scale-125.png
    #	images/BadgeLogo.scale-150.png
    #	images/BadgeLogo.scale-200.png
    #	images/BadgeLogo.scale-400.png
    #	images/LargeTile.scale-100.png
    #	images/LargeTile.scale-125.png
    #	images/LargeTile.scale-150.png
    #	images/LargeTile.scale-200.png
    #	images/LargeTile.scale-400.png
    #	images/SmallTile.scale-100.png
    #	images/SmallTile.scale-125.png
    #	images/SmallTile.scale-150.png
    #	images/SmallTile.scale-200.png
    #	images/SmallTile.scale-400.png
    #	images/Square150x150Logo.scale-100.png
    #	images/Square150x150Logo.scale-125.png
    #	images/Square150x150Logo.scale-150.png
    #	images/Square150x150Logo.scale-200.png
    #	images/Square150x150Logo.scale-400.png
    #	images/Square44x44Logo.altform-unplated_targetsize-16.png
    #	images/Square44x44Logo.altform-unplated_targetsize-256.png
    #	images/Square44x44Logo.altform-unplated_targetsize-32.png
    #	images/Square44x44Logo.altform-unplated_targetsize-48.png
    #	images/Square44x44Logo.scale-100.png
    #	images/Square44x44Logo.scale-125.png
    #	images/Square44x44Logo.scale-150.png
    #	images/Square44x44Logo.scale-200.png
    #	images/Square44x44Logo.scale-400.png
    #	images/Square44x44Logo.targetsize-16.png
    #	images/Square44x44Logo.targetsize-24.png
    #	images/Square44x44Logo.targetsize-24_altform-unplated.png
    #	images/Square44x44Logo.targetsize-256.png
    #	images/Square44x44Logo.targetsize-32.png
    #	images/Square44x44Logo.targetsize-48.png
    #	images/Wide310x150Logo.scale-100.png
    #	images/Wide310x150Logo.scale-125.png
    #	images/Wide310x150Logo.scale-150.png
    #	images/Wide310x150Logo.scale-200.png
    #	images/Wide310x150Logo.scale-400.png
    #	images/splashscreen.scale-100.png
    #	images/splashscreen.scale-125.png
    #	images/splashscreen.scale-150.png
    #	images/splashscreen.scale-200.png
    #	images/splashscreen.scale-400.png
    #	images/storelogo.scale-100.png
    #	images/storelogo.scale-125.png
    #	images/storelogo.scale-150.png
    #	images/storelogo.scale-200.png
    #	images/storelogo.scale-400.png
    #	package.appxmanifest
    #	www/index.html
    #	www/js/init.js
    
    
    Former-commit-id: 1afa26548ba4b7614fa811f2cbb02b1b105e9d9a
    Former-commit-id: 2d7825b5256e0118b354b14f88a06a3bb6427e87
    Former-commit-id: c08f0f464a9b96e2a66b5c27a67611d6c6e30e75

--- www/js/init.js
+++ www/js/init.js
@@ -57,7 +57,7 @@ params['cachedStartPage'] = "Main_Page"; //If you have cached the start page for
 params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/wikivoyage/"; //Include final slash
 
 params['cookieSupport'] = checkCookies();
-params['maxResults'] = ~~(getCookie('maxResults') || 25); //Number of search results to display
+params['maxResults'] = ~~(getCookie('maxResults') || 30); //Number of search results to display
 params['relativeFontSize'] = ~~(getCookie('relativeFontSize') || 100); //Sets the initial font size for articles (as a percentage) - user can adjust using zoom buttons
 params['relativeUIFontSize'] = ~~(getCookie('relativeUIFontSize') || 100); //Sets the initial font size for UI (as a percentage) - user can adjust using slider in Config
 params['cssSource'] = getCookie('cssSource') || "auto"; //Set default to "auto", "desktop" or "mobile"
