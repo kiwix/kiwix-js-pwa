d25fe8f881e6eb2c8db1e119bf534e5d0d927c90
Jaifroid <egk10@cam.ac.uk>
2020-12-30 10:42:53 +0000

    Update NWJS scripts
    Former-commit-id: 99587fd98ad04ec48e022b452e3a59aea83f226c
    Former-commit-id: 0f635a287ddf95bed35a8c5e9bcd51dabc5bd6eb
    Former-commit-id: 57accabe49109f6e900b62ebeb15c56095f99405

--- scripts/Build_NWJS.ps1
+++ scripts/Build_NWJS.ps1
@@ -1,20 +1,25 @@
-$build = "win-x64"
-#$build = "win-ia32"
-$version = "0.48.3"
-#$version = "0.14.7"
+$builds = @("win-x64", "win-ia32", "win-xp")
+$version = "0.50.2"
+$versionXP = "0.14.7"
 $appBuild = "1.1.3N"
 $ZIMbase = "wikipedia_en_100_maxi"
 cd $PSScriptRoot
 cd ..
-$target = "bld\nwjs\$build-$version\kiwix_js_windows-$appBuild"
-$buildLocation = "node_modules\nwjs-builder-phoenix\caches\nwjs-v$version-$build.zip-extracted\nwjs-v$version-$build\"
-$fullTarget = "$target-$build"
-$archiveFolder = "$fullTarget\archives"
-# Remove existing target
-rm $fullTarget\* -Recurse
-md $fullTarget
-# Copy latest binary x64
-cp $buildLocation\* $fullTarget -Recurse
-cp .\package.json, .\pwabuilder-sw.js, .\index.html, .\CHANGELOG.md, .\LICENSE, .\www $fullTarget -Recurse
-md $archiveFolder
-cp "archives\$ZIMbase*.*", "archives\README.md" $archiveFolder
+foreach ($build in $builds) {
+    $target = "bld\nwjs\$build-$version\kiwix_js_windows-$appBuild"
+    $fullTarget = "$target-$build"
+    if ($build -eq "win-xp") {
+        $build = "win-ia32"
+        $version = $versionXP
+    }
+    $buildLocation = "node_modules\nwjs-builder-phoenix\caches\nwjs-v$version-$build.zip-extracted\nwjs-v$version-$build\"
+    $archiveFolder = "$fullTarget\archives"
+    # Remove existing target
+    rm $fullTarget\* -Recurse
+    md $fullTarget
+    # Copy latest binary x64
+    cp $buildLocation\* $fullTarget -Recurse
+    cp .\package.json, .\pwabuilder-sw.js, .\index.html, .\CHANGELOG.md, .\LICENSE, .\www $fullTarget -Recurse
+    md $archiveFolder
+    cp "archives\$ZIMbase*.*", "archives\README.md" $archiveFolder
+}
