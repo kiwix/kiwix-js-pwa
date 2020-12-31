ffde24ac761bf7fb937b75c0b6750b1eb581d029
Jaifroid <egk10@cam.ac.uk>
2020-12-31 07:26:06 +0000

    Interim changelogs
    Former-commit-id: 649204c681de19048f939dcaeedadbf78f7b521a [formerly bbae36b93ef7206fd51dc403e52896629b31f02f]
    Former-commit-id: 4fffb84ddb1c6176187c60e6b9c2b5b7001600b7

--- www/js/init.js
+++ www/js/init.js
@@ -49,7 +49,7 @@ var params = {};
  */
 var appstate = {};
 
-params['version'] = "1.1.3N"; //DEV: Manually update this version when there is a new release: it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "1.1.3-dev"; //DEV: Manually update this version when there is a new release: it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['packagedFile'] = "wikipedia_en_100_maxi.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)  
 params['fileVersion'] = "wikipedia_en_100_maxi_2020-12.zim (23-Dec-2020)"; //Use generic name for actual file, and give version here
