cc555e24f36c16dc8b46bd4344afedb051c2107c
Jaifroid <egk10@cam.ac.uk>
2021-01-02 18:07:10 +0000

    App packages for Kiwix JS Wikivoyage 1.1.3
    Former-commit-id: f5a22d0abdab912529efa9aa86e4bc594ba15aa1
    Former-commit-id: d062d28dacadc2b917820fd69e8c9c498f7c0c5e
    Former-commit-id: 7aeef072f800ccd8a601a9b564933996b82a9206

--- www/js/init.js
+++ www/js/init.js
@@ -49,7 +49,7 @@ var params = {};
  */
 var appstate = {};
 
-params['version'] = "1.1.3 Wikivoyage-dev"; //DEV: Manually update this version when there is a new release: it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "1.1.3 Wikivoyage"; //DEV: Manually update this version when there is a new release: it is compared to the cookie "version" in order to show first-time info, and the cookie is updated in app.js
 params['packagedFile'] = "wikivoyage_en_all_maxi.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)
 params['fileVersion'] = "wikivoyage_en_all_maxi_2020-12 (16-Dec-2020)"; //Use generic name for actual file, and give version here
