be63891d10f6585abb0b51900ea759211c2d4411
Jaifroid <egk10@cam.ac.uk>
2021-01-03 21:45:21 +0000

    Complete Create-DraftRelease script
    Former-commit-id: d6c2de6b12df991c2c6a5f1da1c81ffdc1ca5385 [formerly 8142d600f3efb73ea3b696098b8f466585414fcf] [formerly c1c5e7f034c1dc1d566534bfe85d26f2fb5ea790] [formerly 3598bb862aeb58b758198f59214a1f83c0104320 [formerly 466a1cf2132877870668c0002844d4c0cc28f694 [formerly 03981fbd3d44531b643ce9410ec84ab14cdf2cda]]]
    Former-commit-id: 2a0ed6eff157dddf3af48dfec2d4b46d596f41f9 [formerly d95ed4a572498aaf778e3ffb42744f71b189e891 [formerly 45f4bd817b765ba93c9f9f0697de19974e9d47ba]]
    Former-commit-id: 1cd6b0c5f202fd1fefe1322e72c77bf4c854ae45 [formerly a22fee08b529771401c18dea2a55295aa524d58d]
    Former-commit-id: a664c001e78ab299e9ba0c1b265f4f61692e236b

--- scripts/Create-DraftRelease.ps1
+++ scripts/Create-DraftRelease.ps1
@@ -1,12 +1,23 @@
+param (
+    [string]$tag_name = ""
+)
 
 # Provide parameters
-$tag_name = "v9.8.8-test"
-$release_title = "My new release!"
-$release_body = "This is a test release. Please delete."
-$upload_assets = @('PowerShell.Installation.Script.KiwixWebApp_0.0.0.0_Test.zip')
 $release_uri = 'https://api.github.com/repos/kiwix/kiwix-js-windows/releases'
-$github_token = Get-Content "$PSScriptRoot/github_token" -Raw
+$github_token = Get-Content -Raw "$PSScriptRoot/github_token"
 
+if ($tag_name -eq "") {
+  $tag_name = Read-Host "`nEnter the tag name to use for this release"
+}
+if ($tag_name -NotMatch '^v\d+\.\d+\.\d+') {
+  "`nTag name must be in the format " + '"v0.0.0[-text]"!' + "`n"
+  exit
+}
+"`nCreating release for $tag_name..."
+$base_tag = $tag_name -replace '^v([\d.]+)', '$1'
+$release_title = "Kiwix JS Windows $base_tag UWP"
+$release_body = Get-Content -Raw "$PSScriptRoot/Kiwix_JS_Windows_Release_Body.md"
+$release_body = $release_body -replace '<<base_tag>>', "$base_tag"
 # Set up release_params object
 $release_params = @{
   Uri = $release_uri
@@ -29,34 +40,82 @@ $release = Invoke-RestMethod @release_params
 
 # Check that we appear to have created a release
 if ($release.assets_url -imatch '^https:') {
+  "The draft release details were successfully created.`n"
+  "Searching for assets..."
+  $AppxBundle = dir "$PSScriptRoot/../AppPackages/*_$base_tag*_Test/*_$base_tag*.appx*"
+  # Check the file exists and it's of the right type
+  if ($AppxBundle -and ($AppxBundle.count -eq 1) -and (Test-Path $AppxBundle -PathType leaf) -and 
+    ($AppxBundle -imatch '(.*)\.(?:appx|appxbundle|appxupload)$')) {
+      "Setting main bundle file to $AppxBundle..."
+  } elseif ($AppxBundle.count -ge 2) {
+      "More than one file matches that tag!"
+      return
+  } else {
+      "No package matching that tag was found. Aborting."
+      return
+  }
+  # ZIP the remaining assets
+  "Compressing remaining assets..."
+  $compressed_assets_dir = $AppxBundle -replace '[^/\\]+$', ''
+  $compressed_archive = $compressed_assets_dir + "PowerShell.Installation.Script.KiwixWebApp_$base_tag.0_Test.zip"
+  $AddAppPackage = $compressed_assets_dir + "Add-AppDevPackage*.*"
+  $cert_file = $AppxBundle -replace '\.[^.]+$', '.cer'
+  "Compressing: $AddAppPackage, $cert_file"
+  "$AddAppPackage", "$cert_file" | Compress-Archive -DestinationPath $compressed_archive -Force
+  # Check the compressed file exists
+  if (Test-Path $compressed_archive -PathType leaf) {
+    "Compression successful`n"
+  } else {
+    "There was an error compressing assets."
+    return
+  }
+
+  $upload_assets = @($compressed_archive, $AppxBundle)
   $upload_uri = $release.upload_url -ireplace '\{[^{}]+}', '' 
   "Uploading assets to: $upload_uri..."
-  $upload_file = "$PSScriptRoot\" + $upload_assets[0]
-  # Establish upload params
-  $upload_params = @{
-    Uri = $upload_uri + "?name=" + $upload_assets[0]
-    Method = 'POST'
-    Headers = @{
-      'Authorization' = "token $github_token"
-      'Accept' = 'application/vnd.github.everest-preview+json'
+  
+  ForEach($asset in $upload_assets) {
+    $asset_name = $asset -replace '^.*[\\/]([^\\/]+)$', '$1'
+    # Establish upload params
+    $upload_params = @{
+      Uri = $upload_uri + "?name=$asset_name"
+      Method = 'POST'
+      Headers = @{
+        'Authorization' = "token $github_token"
+        'Accept' = 'application/vnd.github.everest-preview+json'
+      }
+      # Body = [System.IO.File]::ReadAllBytes($upload_file)
+      InFile = $asset
+      ContentType = 'application/octet-stream'
     }
-    # Body = [System.IO.File]::ReadAllBytes($upload_file)
-    InFile = $upload_file
-    ContentType = 'application/octet-stream'
+    "`nUpploading $asset..."
+    # Upload asset to the release server
+    # $upload = [System.IO.File]::ReadAllBytes($upload_file) | Invoke-RestMethod @upload_params
+    $upload = Invoke-RestMethod @upload_params
+    if ($upload.url -imatch '^https:') {
+      "Upload successfully posted as " + $upload.url
+      "Full details:"
+      echo $upload
+    } else {
+      "The upload appears to have failed!"
+      if ($upload) {
+        "The server returned:"
+        echo $upload
+      } else {
+        "The server did not respond."
+      }
+      return
+    }
   }
-  echo $upload_params
-  # Upload asset to the release server
-  # $upload = [System.IO.File]::ReadAllBytes($upload_file) | Invoke-RestMethod @upload_params
-  $upload = Invoke-RestMethod @upload_params
-  if ($upload.url -imatch '^https:') {
-    "`nUpload successfully posted as " + $upload.url
-    "`nFull details:"
-    echo $upload
-    "Done."
-  } else {
-    "The upload appears to have failed. The response was:"
-    echo $upload
-  }
+  "Cleaning up..."
+  del $compressed_archive
+  "`nDone."
 } else {
-  "There was an error setting up the release! The server returned:\n\n$release"
+  "There was an error setting up the release!"
+  if ($release) {
+    "The server returned:"
+    echo $release
+  } else {
+    "The server did not respond."
+  }
 }
---
+++ scripts/Kiwix_JS_Windows_Release_Body.md
@@ -0,0 +1,39 @@
+## This is the Windows 10 UWP app (Store or manual installation)
+
+**This release is intended for the Microsoft Store.** If you are running a previous version of Windows (XP/Vista/7/8), then you can use the [NWJS release instead](https://kiwix.github.io/kiwix-js-windows/kiwix-js-nwjs.html). If you are running Windows 10, then it is easiest to install this app from the Store here:
+
+https://www.microsoft.com/store/apps/9P8SLZ4J979J 
+
+**Status of Store App: IN CERTIFICATION**
+
+If you prefer not to use the Microsoft Store or wish to test a [different build](https://github.com/kiwix/kiwix-js-windows/tree/master/AppPackages), please follow the instructions below the screenshot.
+
+![image](https://user-images.githubusercontent.com/4304337/100858968-91203080-3486-11eb-88c9-baa7f7f7d97d.png)
+
+
+## Manual Installation: Windows 10 Tablet / PC
+
+**Uninstall any previous installation of Kiwix JS Windows (UWP) before you follow this procedure**.
+
+Download the signed bundle [KiwixWebApp_<<base_tag>>.0_AnyCPU.appxbundle](https://github.com/kiwix/kiwix-js-windows/releases/download/v<<base_tag>>/KiwixWebApp_<<base_tag>>.0_AnyCPU.appxbundle) and run/launch it (depending on the browser, you may need to single-click or double-click the file or "download complete" notification).  The App Installer should open and ask you whether you wish to install (it may also ask you if you wish to allow sideloading apps: if so, answer yes). If you are satisfied that the app is trusted, click Install.
+
+On slightly older versions of Windows 10, you may need to enable sideloading of apps beforehand: go to Settings / Update and security / For developers and select "Sideload apps". You may be asked to reboot.
+
+If the above procedures fail, or the App Installer is not available, then download and unzip into a folder the `PowerShell.Installation.Script...` from Assets, place the `.appxbundle` in the same folder, and follow PowerShell Installation instructions [here](https://github.com/kiwix/kiwix-js-windows/tree/master/AppPackages#windows-10-tablet--pc) (except that you don't need to download further files, all the needed ones are included in the zip file `PowerShell.Installation.Script...`).
+
+In case you are still unable to sideload, try the [NWJS release](https://kiwix.github.io/kiwix-js-windows/kiwix-js-nwjs.html) if you cannot use the Store.
+
+## Manual Installation: Windows 10 Mobile
+
+ **Uninstall any previous installation of Kiwix JS Windows (UWP) before you follow this procedure**.
+
+Download [KiwixWebApp_<<base_tag>>.0_AnyCPU.appxbundle](https://github.com/kiwix/kiwix-js-windows/releases/download/v<<base_tag>>/KiwixWebApp_<<base_tag>>.0_AnyCPU.appxbundle) to your downloads folder, or other accessible folder, on the phone. Enable Developer mode on your device (Settings / Updates and security / For developers). 
+
+Open the File Explorer on the phone, navigate to the downloads folder and tap the KiwixWebApp bundle. You will be asked if you wish to install, but it then installs silently in the background. Be patient: it can take a minute or so for the Kiwix icon to appear in the All Apps list. You will also need to download a ZIM file to use with the app (see below).
+
+## Download a ZIM archive (all platforms)
+
+You will need a ZIM file to work with this app. For testing, it comes packaged with an archive of the top 100 Wikipedia articles in English.
+You can download other ZIM archives from the Configuration page in the app (the download completes in the browser). Place the file in an accessible location on your device, and use the Select Storage button in the app to rescan storage and display buttons that let you pick the file or the file's folder.
+
+Alternatively, you can download ZIM archives from http://wiki.kiwix.org/wiki/Content_in_all_languages on a regular PC and (if you are using an external device like a mobile) transfer them to your device with a USB cable.
