8eb86fa0bd88512af8aea2cf3f6fa88cba07854a
Jaifroid <egk10@cam.ac.uk>
2021-02-09 22:34:13 +0000

    Refine logic of failure to load SW mode
    Former-commit-id: fdb943e2a6ba0f87e015e75396bbe365261e3b11 [formerly 739ceb1cbd5285f6046c13d001078aa897319aa3] [formerly 318812b394acbe4b9d0e1ff7512b6167dc3d2f81] [formerly 405a02166eb4a768fb606cffc0400d1942a03d9f [formerly 053de777747d2ed5429ae953e9e1754dd006b195 [formerly 3582f2971c4ce54692d7d968b09147c6f53b8170]]]
    Former-commit-id: 39b954f4ca8cf54f8e387878ff56c788f585a89c [formerly 7da957a350b359b868f5454a45ce4987904cdb66 [formerly 6cc27773e60aebecc52e7e8f90323f9c92568a27]]
    Former-commit-id: b8044ce7ba81a0533e1130e33490ffc9deb593ae [formerly a388afea7cb8494a939d1e1bffcdc001e07f15f7]
    Former-commit-id: dfb52fb94023f4c74db5016d956306cb072f0045

--- www/js/app.js
+++ www/js/app.js
@@ -1578,7 +1578,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'cache', 'images', 'sett
                                 // in case it has been stopped and lost its context
                                 initOrKeepAliveServiceWorker();
                             }
-                        }, function (err) {
+                        }).catch(function (err) {
                             console.error('error while registering serviceWorker', err);
                             refreshAPIStatus();
                             var goPWA = false;
@@ -1597,9 +1597,6 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'cache', 'images', 'sett
                                     'periodically when online as per the Service Worker spec.\n\n' +
                                     'You can switch back any time by toggling "Allow Internet access?" off.\n\n' +
                                     'WARNING: This will attempt to access the following server: \n' + params.PWAServer;
-                                goPWA = true;
-                            }
-                            if (goPWA) {
                                 var launchPWA = function () {
                                     settingsStore.setItem('contentInjectionMode', value, Infinity);
                                     // This is needed so that we get passthrough on subsequent launches
@@ -1625,12 +1622,10 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'cache', 'images', 'sett
                                 if (settingsStore.getItem('allowInternetAccess') === 'true' && params.localUWPSettings.PWA_launch !== 'fail') {
                                     checkPWAIsOnline();
                                     return;
-                                } else {
-                                    if (params.localUWPSettings.PWA_launch === 'fail') {
-                                        message = 'WARNING: The PWA failed to launch on the last attempt!' +
-                                            '\n\nTo remain in local mode select Cancel,\n' +
-                                            'or you can try to launch the PWA again by selecting Access server:';
-                                    }
+                                } else if (params.localUWPSettings.PWA_launch === 'fail') {
+                                    message = 'WARNING: The PWA failed to launch on the last attempt!' +
+                                        '\n\nTo remain in local mode select Cancel,\n' +
+                                        'or you can try to launch the PWA again by selecting Access server:';
                                     uiUtil.systemAlert(message, 'Warning!', 'Access server', checkPWAIsOnline, 'Cancel', function () {
                                         var allowAccessCheck = document.getElementById('allowInternetAccessCheck');
                                         if (allowAccessCheck.checked) allowAccessCheck.click();
