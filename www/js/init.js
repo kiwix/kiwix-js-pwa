60e52447c1080998542c90f699f4c2ca390ee894
Jaifroid <egk10@cam.ac.uk>
2021-04-13 14:18:07 +0100

    App packages for Kiwix JS Windows 1.2.4
    Former-commit-id: 06505d430d496c81f22e575d812dddb3cb0ba3b2 [formerly 1bb257815668bc77d334a96de6214217578b500e]
    Former-commit-id: 9ca3903a44380582865790806edd3d6911dfa5e4

--- www/js/init.js
+++ www/js/init.js
@@ -49,11 +49,11 @@ var params = {};
  */
 var appstate = {};
 /******** UPDATE VERSION IN pwabuilder-sw.js TO MATCH VERSION *******/
-params['version'] = "1.2.4-RC1"; //DEV: Manually update this version when there is a new release: it is compared to the Settings Store "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "1.2.4"; //DEV: Manually update this version when there is a new release: it is compared to the Settings Store "version" in order to show first-time info, and the cookie is updated in app.js
 /******* UPDATE THIS ^^^^^^ IN serveice worker!! ********************/
 params['packagedFile'] = getSetting('packagedFile') || "wikipedia_en_100_maxi.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)  
-params['fileVersion'] = getSetting('fileVersion') || "wikipedia_en_100_maxi_2021-02.zim (23-Feb-2021)"; //This will be displayed in the app - optionally include date of ZIM file
+params['fileVersion'] = getSetting('fileVersion') || "wikipedia_en_100_maxi_2021-03.zim (26-Mar-2021)"; //This will be displayed in the app - optionally include date of ZIM file
 // List of known start pages cached in the FS:
 params['cachedStartPages'] = {
     'wikipedia_en_medicine-app_maxi': 'A/Wikipedia:WikiProject_Medicine/Open_Textbook_of_Medicine',
