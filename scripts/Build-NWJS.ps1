b428ef1629423c964d078ddad9750105dd31ec55
Jaifroid <egk10@cam.ac.uk>
2021-04-20 16:07:32 +0100

    Update release scripts
    Former-commit-id: 1f90922a5a62cd910dd9e099c910dc7e6242e9c9 [formerly bf4d11ef3e3ed2bed7146718f2b379536fd66368]
    Former-commit-id: 481a651e5b8c3c454f806e790afbc9fa56e55452

--- scripts/Build-NWJS.ps1
+++ scripts/Build-NWJS.ps1
@@ -1,7 +1,7 @@
 $builds = @("win-x64", "win-ia32", "win-xp")
 $version = "0.52.2"
 $versionXP = "0.14.7"
-$appBuild = "1.2.4N"
+$appBuild = "1.2.5N"
 $ZIMbase = "wikipedia_en_100_maxi"
 cd $PSScriptRoot
 cd ..
