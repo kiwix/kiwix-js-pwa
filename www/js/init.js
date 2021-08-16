a8bc9bb6f5a62379feab0b56403b925bf8cd7dfb
Jaifroid <egk10@cam.ac.uk>
2021-08-16 17:12:47 +0300

    Rebase Wikivoyage on master
    Former-commit-id: 76a483b872e6177f0be61068be485e104552d90a
    Former-commit-id: 8bf057269102011bb1d3d5cc7a2607140d25b8fb
    Former-commit-id: 1261238b5b8d47db85e2546de72b76f877154083

--- www/js/init.js
+++ www/js/init.js
@@ -51,16 +51,16 @@ var appstate = {};
 /******** UPDATE VERSION IN pwabuilder-sw.js TO MATCH VERSION AND CHECK PWASERVER BELOW!!!!!!! *******/
 params['version'] = "1.6.2-rc2"; //DEV: Manually update this version when there is a new release: it is compared to the Settings Store "version" in order to show first-time info, and the cookie is updated in app.js
 /******* UPDATE THIS ^^^^^^ IN service worker AND PWA-SERVER BELOW !! ********************/
-params['packagedFile'] = getSetting('packagedFile') || "wikipedia_en_100_maxi_2021-07.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
-params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)  
-params['fileVersion'] = getSetting('fileVersion') || "wikipedia_en_100_maxi_2021-07.zim (28-July-2021)"; //This will be displayed in the app - optionally include date of ZIM file
+params['packagedFile'] = getSetting('packagedFile') || "wikivoyage_en_all_maxi_2021-07.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)
+params['fileVersion'] = getSetting('fileVersion') || "wikivoyage_en_all_maxi_2021-07 (8-July-2021)"; //This will be displayed in the app - optionally include date of ZIM file
 // List of known start pages cached in the FS:
 params['cachedStartPages'] = {
     'wikipedia_en_medicine-app_maxi': 'A/Wikipedia:WikiProject_Medicine/Open_Textbook_of_Medicine',
     'wikipedia_en_medicine_maxi': 'A/Wikipedia:WikiProject_Medicine/Open_Textbook_of_Medicine',
     'wikivoyage_en_all_maxi': 'A/Main_Page'
 };
-params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/"; //Include final slash
+params['kiwixDownloadLink'] = "https://download.kiwix.org/zim/wikivoyage/"; //Include final slash
 /******* DEV: ENSURE SERVERS BELOW ARE LISTED IN package.appxmanifest ************/
 params['PWAServer'] = "https://pwa.kiwix.org/"; // Production server
 // params['PWAServer'] = "https://kiwix.github.io/kiwix-js-windows/"; // Test server
@@ -83,7 +83,7 @@ params['hideToolbars'] = getSetting('hideToolbars') != null ? getSetting('hideTo
 params['rememberLastPage'] = getSetting('rememberLastPage') != null ? getSetting('rememberLastPage') : true; //Set default option to remember the last visited page between sessions
 params['useMathJax'] = getSetting('useMathJax') != null ? getSetting('useMathJax') : true; //Set default to true to display math formulae with MathJax, false to use fallback SVG images only
 //params['showFileSelectors'] = getCookie('showFileSelectors') != null ? getCookie('showFileSelectors') : false; //Set to true to display hidden file selectors in packaged apps
-params['showFileSelectors'] = true; //False will cause file selectors to be hidden on each load of the app (by ignoring cookie)
+params['showFileSelectors'] = false; //False will cause file selectors to be hidden on each load of the app (by ignoring cookie)
 params['hideActiveContentWarning'] = getSetting('hideActiveContentWarning') != null ? getSetting('hideActiveContentWarning') : false;
 params['allowHTMLExtraction'] = getSetting('allowHTMLExtraction') == true;
 params['alphaChar'] = getSetting('alphaChar') || 'A'; //Set default start of alphabet string (used by the Archive Index)
