e89d28197f530ddac47754d744b5ac3170eef1a3
Jaifroid <egk10@cam.ac.uk>
2021-08-17 15:55:40 +0300

    Update packaged ZIM to wikivoyage_en_all_maxi_2021-08
    Former-commit-id: 76ae64a39d9b5a1174fd28610029497462142614
    Former-commit-id: 0132956bfe6626db83c15a4ea4ed5c9a3b58becf
    Former-commit-id: 9da32c5cd369629ae3a47d16fe2d0e59c5bee8e6

--- www/js/init.js
+++ www/js/init.js
@@ -49,11 +49,11 @@ var params = {};
  */
 var appstate = {};
 /******** UPDATE VERSION IN pwabuilder-sw.js TO MATCH VERSION AND CHECK PWASERVER BELOW!!!!!!! *******/
-params['version'] = "1.6.2-rc2"; //DEV: Manually update this version when there is a new release: it is compared to the Settings Store "version" in order to show first-time info, and the cookie is updated in app.js
+params['version'] = "1.6.2-Wikivoyage"; //DEV: Manually update this version when there is a new release: it is compared to the Settings Store "version" in order to show first-time info, and the cookie is updated in app.js
 /******* UPDATE THIS ^^^^^^ IN service worker AND PWA-SERVER BELOW !! ********************/
-params['packagedFile'] = getSetting('packagedFile') || "wikivoyage_en_all_maxi_2021-07.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['packagedFile'] = getSetting('packagedFile') || "wikivoyage_en_all_maxi_2021-08.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)
-params['fileVersion'] = getSetting('fileVersion') || "wikivoyage_en_all_maxi_2021-07 (8-July-2021)"; //This will be displayed in the app - optionally include date of ZIM file
+params['fileVersion'] = getSetting('fileVersion') || "wikivoyage_en_all_maxi_2021-08 (8-August-2021)"; //This will be displayed in the app - optionally include date of ZIM file
 // List of known start pages cached in the FS:
 params['cachedStartPages'] = {
     'wikipedia_en_medicine-app_maxi': 'A/Wikipedia:WikiProject_Medicine/Open_Textbook_of_Medicine',
