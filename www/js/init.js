a9e5b4cdd9059886b18623c03b7d4f2e58974c47
Jaifroid <egk10@cam.ac.uk>
2021-08-30 08:59:35 +0100

    Add double right-click option
    Former-commit-id: b9ad1437e22dbdee0da668b670289533e3fbc130 [formerly 77bfad1d1507b991fe6bc63194b2fc06a8cc6092 [formerly 9a99a20dee5602c38d4730ec212623b1de0c84fd]]
    Former-commit-id: 121eb4341b526db0e3a949b0e9076fc29112fc24
    Former-commit-id: a6d059860aafb65654718bf46103c22fc6675182

--- www/js/init.js
+++ www/js/init.js
@@ -91,6 +91,7 @@ params['omegaChar'] = getSetting('omegaChar') || 'Z'; //Set default end of alpha
 params['contentInjectionMode'] = getSetting('contentInjectionMode') || 'jquery'; // Defaults to jquery mode (widest compatibility)
 params['allowInternetAccess'] = getSetting('allowInternetAccess');
 params['windowOpener'] = getSetting('windowOpener'); // 'tab|window|false' A setting that determines whether right-click/long-press of a ZIM link opens a new window/tab
+params['rightClickType'] = getSetting('rightClickType'); // 'single|double|false' A setting that determines whether a single or double right-click is used to open a new window/tab
 params['navButtonsPos'] = getSetting('navButtonsPos') || 'bottom'; // 'top|bottom' A setting that determines where the back-forward nav buttons appear
 
 //Do not touch these values unless you know what they do! Some are global variables, some are set programmatically
