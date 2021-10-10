d200b1f2e741a50fb9c03db778cef88acbc834c3
Jaifroid <egk10@cam.ac.uk>
2021-10-10 15:24:49 +0100

    Use smaller map pin for Wikipedia vs Wikivoyage
    Former-commit-id: a4e7f14c9b9cc27dfef5a4786ebe83c0b998f3d1

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -53,6 +53,7 @@ let precacheFiles = [
   "www/img/icons/wikivoyage-black-32.png",
   "www/img/icons/wikivoyage-white-32.png",
   "www/img/icons/map_marker-30px.png",
+  "www/img/icons/map_marker-18px.png",
   "www/img/spinner.gif",
   "www/index.html",
   "www/article.html",
