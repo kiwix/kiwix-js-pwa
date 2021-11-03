62740b573bf23eae842e20fea9708d278d106979
Jaifroid <egk10@cam.ac.uk>
2021-11-03 17:16:13 +0000

    Update Build-DockerContainer.ps1

--- scripts/Build-DockerContainer.ps1
+++ scripts/Build-DockerContainer.ps1
@@ -1,55 +1,69 @@
 param (
-    [string]$tag_name = "",
+    [string]$machine_name = "",
+    [string]$branch_name = "",
     [switch]$dryrun = $false
 )
 
-# DEV: To build the Docker container with a masnual dispatch (for testing only), provide an existing tag_name on master
+# DEV: To build the Docker container programmatically (for testing), provide the -machine_name (this could be 'dev')
+# and the -branch_name switches on the commandline. If you do not provide them, you will be prompted with defaults.
+# IMPORTANT: Ensure that your personal github token is in your local copy of the '/scripts' directory, saved as 'github_token'
 
 # Provide parameters
 $release_uri = 'https://api.github.com/repos/kiwix/kiwix-js-windows/actions/workflows/publish-docker.yaml/dispatches'
 $github_token = Get-Content -Raw "$PSScriptRoot/github_token"
 
-$init_params = Get-Content -Raw "$PSScriptRoot\..\www\js\init.js"
-$pwabuilder = Get-Content -Raw "$PSScriptRoot\..\pwabuilder-sw.js"
+$app_params = Select-String 'appVersion' "$PSScriptRoot\..\www\js\init.js" -List
+$serviceworker = Select-String 'appVersion' "$PSScriptRoot\..\pwabuilder-sw.js" -List
 $suggested_build = ''
-$init_tag = ''
-if ($init_params -match 'params\[[''"]version[''"]]\s*=\s*[''"]([^''"]+)') {
-  $init_tag = $matches[1]
-  $suggested_build = 'v' + $init_tag 
+$app_tag = ''
+if ($app_params -match 'params\[[''"]appVersion[''"]]\s*=\s*[''"]([^''"]+)') {
+  $app_tag = $matches[1]
+  $suggested_build = 'dev_' + $app_tag 
+} else {
+  "*** WARNING: App version is incorrectly set in init.js.`nPlease correct before continuing.`n"
+  exit
 }
-$pwabuilder_tag = ''
-if ($pwabuilder -match 'appVersion\s*=\s*[''"]([^''"]+)') {
-  $pwabuilder_tag = $matches[1]
-  if ($pwabuilder_tag -ne $init_tag) {
-    "`n*** WARNING: The tag in init.js [$init_tag] does not match the tag in pwabuilder-sw.js [$pwabuilder_tag]! ***"
+$sw_tag = ''
+if ($serviceworker -match 'appVersion\s*=\s*[''"]([^''"]+)') {
+  $sw_tag = $matches[1]
+  if ($sw_tag -ne $app_tag) {
+    "*** WARNING: The version in init.js [$app_tag] does not match the version in pwabuilder-sw.js [$sw_tag]! ***"
     "Please correct before continuing.`n"
     exit
+  } else {
+    "Version in init.js: $app_tag"
+    "Version in pwabuilder-sw.js: $sw_tag`n"
   }
+} else {
+  "*** WARNING: App version is incorrectly set in pwabuilder-sw.js.`nPlease correct before continuing.`n"
+  exit
 }
 
-if ($tag_name -eq "") {
-  $tag_name = Read-Host "`nGive the tag name to use for the docker build, or Enter to accept suggested tag, or add any suffix to suggested tag [$suggested_build]"
-  if ($tag_name -match '^[EN-]|^$') {
-    $split = $suggested_build -imatch '^([v\d.]+)(.*)$'
-    if ($split) {
-      $tag_name = $matches[1] + $tag_name + $matches[2]
-      # Clean up in case there was already a WikiMed or Wikivoyage suffix and we added one
-      $tag_name = $tag_name -replace '(\-[^\d.-]+)\-[^\d.]+$', '$1'
-    }
-    "Tag name set to: $tag_name"
-  }
+if ($machine_name -eq "") {
   if (-Not $dryrun) {
     $dryrun_check = Read-Host "Is this a dry run? [Y/N]"
     $dryrun = -Not ( $dryrun_check -imatch 'n' )
     If ($dryrun) {
-      "Initiating dry run..."
+      "[DRYRUN]: Initiating dry run..."
     }
   }
+  $machine_name = Read-Host "`nGive the tag name to use for the docker build, or Enter to accept suggested build name [$suggested_build]"
+  if (-Not $machine_name) { $machine_name = $suggested_build }
 }
-if ($tag_name -NotMatch '^v\d+\.\d+\.\d+([EN-]|$)') {
-    "`nTag name must be in the format " + '"v0.0.0[E][N][-text]"!' + "`n"
-    exit
+
+if ($branch_name -eq "") {
+  $suggested_branch = &{ git branch --show-current }
+  $branch_name = Read-Host "Give the branch name to use of the docker build, or Enter to accept [$suggested_branch]"
+  if (-Not $branch_name) { $branch_name = $suggested_branch }
+  if ($branch_name -imatch '^pr/\d+') {
+    "`nWARNING: You appear to have indicated a PR. Please check out the underlying branch to use this script,`nor else run it again and give the branch name at the prompt.`n"
+    return
+  }
+
 }
+
+"`nTag name set to: $machine_name"
+"Branch name set to: $branch_name"
 
 # Set up dispatch_params object - for API see https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
 $dispatch_params = @{
@@ -60,18 +74,19 @@ $dispatch_params = @{
       'Accept' = 'application/vnd.github.v3+json'
     }
     Body = @{
-      'ref' = "master"
-      'inputs' = @{ 'version' = $tag_name }
+      'ref' = $branch_name
+      'inputs' = @{ 'version' = $machine_name }
     } | ConvertTo-Json
     ContentType = "application/json"
 }
+
+$dispatch_f = ($dispatch_params | Format-List | Out-String);
+"`nDispatch parameters:`n$dispatch_f"
   
 # Post to the release server
-if (-Not ($dryrun -or $buildonly -or $updatewinget)) { 
-    $dispatch = Invoke-RestMethod @dispatch_params 
-} elseif (-Not $updatewinget) {
-    "[DRYRUN] Dispatch parameters:`n$dispatch_params`n$tag_name"
-    return
+if (-Not $dryrun) { 
+  Invoke-RestMethod @dispatch_params 
+  "`nCheck for any error message above. An empty dispatch is normal, and indicates that the command was accepted.`n"
+} else {
+  "[DRYRUN]: Complete.`n"
 }
-"`nServer returned: $dispatch"
-"`nAn empty dispatch is normal, and indicates that the command was accepted.`n"
