a968e49c8bbf98d8a413eeff7b776a95de093fd0
Jaifroid <egk10@cam.ac.uk>
2021-12-27 08:26:36 +0000

    Make SW upgrade happen only on next restart

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,14 +1,16 @@
 ﻿// Self-destroying service-worker - see https://github.com/NekR/self-destroying-sw
-self.addEventListener('install', function(e) {
-    self.skipWaiting();
+
+// If you want new SW to activate immediately, use below
+// self.addEventListener('install', function (e) {
+//   self.skipWaiting();
+// });
+
+self.addEventListener('activate', function (e) {
+  self.registration.unregister().then(function () {
+    return self.clients.matchAll();
+  }).then(function (clients) {
+    clients.forEach(function (client) {
+      return client.navigate(client.url);
+    });
   });
-  
-  self.addEventListener('activate', function(e) {
-    self.registration.unregister()
-      .then(function() {
-        return self.clients.matchAll();
-      })
-      .then(function(clients) {
-        clients.forEach(client => client.navigate(client.url))
-      });
-  });
+});
