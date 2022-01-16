d9f257521d5fc339a1e3400e95aa5c9154183c9d
Jaifroid <egk10@cam.ac.uk>
2022-01-16 13:48:56 +0000

    Backport developer reset methods from kiwix js (#227)
    Former-commit-id: 6439eabf291d227b6965d914bde7aa8096ca1713 [formerly e30b39a9cf0cb9ec84a24e90a60c2909202b5bd3] [formerly f3474e8724f5a8daffd498fda2aef6ae44ede0bc] [formerly 68a0ec12615b6efd9a3061594d8e2d96b505341f [formerly f9a193b804db8ce40c67deeeadaacc34ea70479f [formerly 25a67567a16378afd25ab2d090b3adece7852d77]]]
    Former-commit-id: 4d201b57ae8d3683a8f969fce6e1030de583423f [formerly e9ade7a5df2168bc9fd03cf50ca8720f096a0ed5 [formerly 68cd10b143b1ff9501a1d01038bdb27d3cd917f2]]
    Former-commit-id: 4594b69606e0144deea964dbd20db50f421f0c79 [formerly 9e2729e51140b44b3666737b59558f483d3adab6]
    Former-commit-id: ba36b6d226bf9a318ffbdbb78d2c42a9c0e160a7

--- www/js/lib/settingsStore.js
+++ www/js/lib/settingsStore.js
@@ -46,7 +46,7 @@ define([], function () {
    * @type {Array}
    */
   var deprecatedKeys = [
-    'lastContentInjectionMode', 'lastPageHTML', 'lastPageVisit', 'version'
+    'lastContentInjectionMode', 'lastPageHTML', 'lastPageVisit', 'version', 'useCache'
   ];
 
   /**
@@ -93,6 +93,142 @@ define([], function () {
     // Note that if this function returns 'none', the cookie implementations below will run anyway. This is because storing a cookie
     // does not cause an exception even if cookies are blocked in some contexts, whereas accessing localStorage may cause an exception
     return type;
+  }
+
+  /**
+   * Performs a full app reset, deleting all caches and settings
+   * Or, if a parameter is supplied, deletes or disables the object
+   * @param {String} object Optional name of the object to disable or delete ('cookie', 'localStorage', 'cacheAPI')
+   */
+  function reset(object) {
+    var performReset = function () {
+      // 1. Clear any cookie entries
+      if (!object || object === 'cookie') {
+        var regexpCookieKeys = /(?:^|;)\s*([^=]+)=([^;]*)/ig;
+        var currentCookie = document.cookie;
+        var foundCrumb = false;
+        var cookieCrumb = regexpCookieKeys.exec(currentCookie);
+        while (cookieCrumb !== null) {
+          // DEV: Note that we don't use the keyPrefix in legacy cookie support
+          foundCrumb = true;
+          // This expiry date will cause the browser to delete the cookie crumb on next page refresh
+          document.cookie = cookieCrumb[1] + '=;expires=Thu, 21 Sep 1979 00:00:01 UTC;';
+          cookieCrumb = regexpCookieKeys.exec(currentCookie);
+        }
+        if (foundCrumb) console.debug('All cookie keys were expired...');
+      }
+
+      // 2. Clear any localStorage settings
+      if (!object || object === 'localStorage') {
+        if (/localStorage/.test(assetsCache.capability)) {
+          localStorage.clear();
+          console.debug('All Local Storage settings were deleted...');
+        }
+      }
+
+      // 3. Clear any IndexedDB entries
+      if (!object || object === 'indexedDB') {
+        if (/indexedDB/.test(assetsCache.capability)) {
+          var cache = require('cache');
+          cache.clear('reset');
+        }
+      }
+
+      // 4. Clear any (remaining) Cache API caches
+      if (!object || object === 'cacheAPI') {
+        getCacheNames(function (cacheNames) {
+          if (cacheNames && !cacheNames.error) {
+            var cnt = 0;
+            for (var cacheName in cacheNames) {
+              cnt++;
+              caches.delete(cacheNames[cacheName]).then(function () {
+                cnt--;
+                if (!cnt) {
+                  // All caches deleted
+                  console.debug('All Cache API caches were deleted...');
+                  // Reload if user performed full reset or if appCache is needed
+                  if (!object || params.appCache) _reloadApp();
+                }
+              });
+            }
+          } else {
+            console.debug('No Cache API caches were in use (or we do not have access to the names).');
+            // All operations complete, reload if user performed full reset or if appCache is needed
+            if (!object || params.appCache) _reloadApp();
+          }
+        });
+      }
+    };
+    var uiUtil = require('uiUtil');
+    // If no specific object was specified, we are doing a general reset, so ask user for confirmation
+    if (object) performReset();
+    else {
+      uiUtil.systemAlert('WARNING: This will reset the app to a freshly installed state, deleting all app caches and settings!',
+      'Warning!', 'OK', performReset, 'Cancel',
+      function () {
+        console.debug('User cancelled');
+      });
+    }
+  }
+
+  // Gets cache names from Service Worker, as we cannot rely on having them in params.cacheNames
+  function getCacheNames(callback) {
+    if (navigator.serviceWorker && navigator.serviceWorker.controller) {
+      var channel = new MessageChannel();
+      channel.port1.onmessage = function (event) {
+        var names = event.data;
+        callback(names);
+      };
+      navigator.serviceWorker.controller.postMessage({
+        action: 'getCacheNames'
+      }, [channel.port2]);
+    } else {
+      callback(null);
+    }
+  }
+
+  // Deregisters all Service Workers and reboots the app
+  function _reloadApp() {
+    var reboot = function () {
+      console.debug('Performing app reload...');
+      setTimeout(function () {
+        window.location.href = location.origin + location.pathname + uriParams 
+      }, 300);
+    };
+    // Blank the querystring, so that parameters are not set on reload
+    var uriParams = '';
+    if (~window.location.href.indexOf(params.PWAServer) && params.referrerExtensionURL) {
+      // However, if we're in a PWA that was called from local code, then by definition we must remain in SW mode and we need to
+      // ensure the user still has access to the referrerExtensionURL (so they can get back to local code from the UI)
+      uriParams = '?allowInternetAccess=truee&contentInjectionMode=serviceworker';
+      uriParams += '&referrerExtensionURL=' + encodeURIComponent(params.referrerExtensionURL);
+    }
+    if (navigator && navigator.serviceWorker) {
+      console.debug('Deregistering Service Workers...');
+      var cnt = 0;
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        if (!registrations.length) {
+          reboot();
+          return;
+        }
+        cnt++;
+        registrations.forEach(function (registration) {
+          registration.unregister().then(function () {
+            cnt--;
+            if (!cnt) {
+              console.debug('All Service Workers unregistered...');
+              reboot();
+            }
+          });
+        });
+      }).catch(function (err) {
+        console.error(err);
+        reboot();
+      });
+    } else {
+      console.debug('Performing app reload...');
+      reboot();
+    }
   }
 
   var settingsStore = {
@@ -183,6 +319,8 @@ define([], function () {
     setItem: settingsStore.setItem,
     removeItem: settingsStore.removeItem,
     hasItem: settingsStore.hasItem,
+    getCacheNames: getCacheNames,
+    reset: reset,
     getBestAvailableStorageAPI: getBestAvailableStorageAPI
   };
 });
