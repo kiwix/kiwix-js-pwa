418a1c456012386abee048b4e3a1d601ccc6306b
Jaifroid <egk10@cam.ac.uk>
2022-01-18 11:48:25 +0000

    Remove JQuery cleaning from SW mode
    Former-commit-id: f6d9ff62a7acd250f98345ae39dc5b1dc3c3bdbd [formerly 6497b03c05f12b3dd95494345393a539cb8c0128]
    Former-commit-id: e50e726a1f4328b8e4a746cda078592bf8aa536b

--- www/js/app.js
+++ www/js/app.js
@@ -3714,11 +3714,11 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'cache', 'images', 'sett
                 maxPageWidthProcessed = false;
                 loaded = false;
                 // Remove those pesky JQuery hooks to prevent memory leaks
-                if (articleContainer.document) {
-                    $(articleContainer.document).contents().remove();    
-                } else {
-                    $(articleContainer).contents().remove();
-                }
+                //if (articleContainer.document) {
+                //    $(articleContainer.document).contents().remove(); // Causes exception in UWP   
+                //} else {
+                //    $(articleContainer).contents().remove();
+                //}
                 // If loading the iframe, we can hide the frame (for UWP apps: for others, the doc should already be
                 // hidden). Note that testing appstate.target is probably redundant for UWP because it will always
                 // be iframe even if an external window is loaded... (but we probably need to do so for other cases)
