797ee22a1baee8efb787efc5da702f5db0015c8b
Jaifroid <egk10@cam.ac.uk>
2022-02-04 11:30:07 +0000

    Further changes to systemAlert from KJS
    Former-commit-id: ff8ba0c6027c3adc4f2ac53d170f2b9302cdf4b4 [formerly dccf72e42880bd9fd92d022fe668db4721b3a690 [formerly 175c2431c20f66ff378b1ee39358e1aff14327d2]]
    Former-commit-id: 8f4c2c3a42131affaa226ad7e049fd73bc29fbd3
    Former-commit-id: 13e2a72a6ddf50ff00bd554e697724d019bc09b5

--- www/js/lib/uiUtil.js
+++ www/js/lib/uiUtil.js
@@ -580,27 +580,36 @@ define(rqDef, function(util) {
      * Credit to @gaurav7019 for this code (slightly adapted here) - see Kiwix JS #804
      * 
      * @param {String} message The alert message to display in the body of the modal 
-     * @param {String} label The modal's label or title which appears in the header 
-     * @param {Boolean} isConfirm If true, the modal will be a confirm dialog box, otherwise it will be an alert 
-     * @param {String} declineButtonText The text to display on the decline button (optional, Default = "Cancel") 
-     * @param {String} approveButtonText  The text to display on the approve button (optional, Default = "Confirm")
-     * @returns {Promise<Boolean>} A promise which resolves to true if the user clicked Confirm, false if the user clicked Cancel, backdrop or the cross(x) button
+     * @param {String} label The modal's label or title which appears in the header (optional, Default = "Confirmation" or "Message")
+     * @param {Boolean} isConfirm If true, the modal will be a confirm dialog box, otherwise it will be a simple alert message 
+     * @param {String} declineConfirmLabel The text to display on the decline confirmation button (optional, Default = "Cancel") 
+     * @param {String} approveConfirmLabel  The text to display on the approve confirmation button (optional, Default = "Confirm")
+     * @param {String} closeMessageLabel  The text to display on the close alert message button (optional, Default = "Okay")
+     * @returns {Promise<Boolean>} A promise which resolves to true if the user clicked Confirm, false if the user clicked Cancel/Okay, backdrop or the cross(x) button
      */
-     function systemAlert(message, label, isConfirm, declineButtonText, approveButtonText) {
-        label = label || (isConfirm ? 'Confirm' : 'Message');
+     function systemAlert(message, label, isConfirm, declineConfirmLabel, approveConfirmLabel, closeMessageLabel) {
+        declineConfirmLabel = declineConfirmLabel || document.getElementById("declineConfirm").textContent;
+        approveConfirmLabel = approveConfirmLabel || document.getElementById("approveConfirm").textContent;
+        closeMessageLabel = closeMessageLabel || document.getElementById("closeMessage").textContent;
+        label = label || (isConfirm ? "Confirmation" : "Message");
         return new Promise(function (resolve, reject) {
-            if (!message) reject("Missing parameters");
-            if (declineButtonText) document.getElementById("declineModal").innerHTML = declineButtonText;
-            if (approveButtonText) document.getElementById("approveModal").innerHTML = approveButtonText;
-            document.getElementById("modalLabel").innerHTML = label;
-            document.getElementById("modalText").innerHTML = message;
-            // Displays an additional Confirm button if isConfirm is true
-            document.getElementById("approveModal").style.visibility = isConfirm ? "visible" : "hidden";
+            if (!message) reject("Missing body message");
+            // Set the text to the modal and it's buttons
+            document.getElementById("approveConfirm").textContent = approveConfirmLabel;
+            document.getElementById("declineConfirm").textContent = declineConfirmLabel;
+            document.getElementById("closeMessage").textContent = closeMessageLabel;
+            document.getElementById("modalLabel").textContent = label;
+            document.getElementById("modalText").textContent = message;
+            // Display buttons acc to the type of alert
+            document.getElementById("approveConfirm").style.display = isConfirm ? "block" : "none";
+            document.getElementById("declineConfirm").style.display = isConfirm ? "block" : "none";
+            document.getElementById("closeMessage").style.display = isConfirm ? "none" : "block";
+            // Display the modal
             $("#alertModal").modal("show");
             // When hide model is called, resolve promise with true if hidden using approve button, false otherwise
             $("#alertModal").on("hide.bs.modal", function () {
                 const closeSource = document.activeElement;
-                if (closeSource.id === "approveModal") {
+                if (closeSource.id === "approveConfirm") {
                     resolve(true);
                 } else {
                     resolve(false);
@@ -608,7 +617,7 @@ define(rqDef, function(util) {
             });
         });
     }
-
+    
     /**
      * Checks if a server is accessible by attempting to load a test image from the server
      * 
