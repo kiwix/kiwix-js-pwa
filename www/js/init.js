9cdd7102ec026e1150b45c1cb10ca0880960cd71
Jaifroid <egk10@cam.ac.uk>
2022-02-28 20:57:29 +0000

    Update to latest WikiMed
    Former-commit-id: e35560fb66d293aa276b52a84dded8673780af51
    Former-commit-id: 60a5a2a988b807d76990d0413b0f3633b5ab2e46
    Former-commit-id: 59e298948c84479fde9ef1361c4f6044db46af0f

--- www/js/init.js
+++ www/js/init.js
@@ -52,9 +52,9 @@ var appstate = {};
 /******** UPDATE VERSION IN service-worker.js TO MATCH VERSION AND CHECK PWASERVER BELOW!!!!!!! *******/
 params['appVersion'] = "1.9.2-WikiMed"; //DEV: Manually update this version when there is a new release: it is compared to the Settings Store "appVersion" in order to show first-time info, and the cookie is updated in app.js
 /******* UPDATE THIS ^^^^^^ IN service worker AND PWA-SERVER BELOW !! ********************/
-params['packagedFile'] = getSetting('packagedFile') || "mdwiki_en_all-app_maxi_2022-02.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
+params['packagedFile'] = getSetting('packagedFile') || "wikipedia_en_medicine_maxi_2022-02.zim"; //For packaged Kiwix JS (e.g. with Wikivoyage file), set this to the filename (for split files, give the first chunk *.zimaa) and place file(s) in default storage
 params['archivePath'] = "archives"; //The directory containing the packaged archive(s) (relative to app's root directory)  
-params['fileVersion'] = getSetting('fileVersion') || "mdwiki_en_all-app_maxi_2022-02 (6-Feb-2022)"; //This will be displayed in the app - optionally include date of ZIM file
+params['fileVersion'] = getSetting('fileVersion') || "wikipedia_en_medicine_maxi_2022-02 (4-Feb-2022)"; //This will be displayed in the app - optionally include date of ZIM file
 // List of known start pages cached in the FS:
 params['cachedStartPages'] = {
     'wikipedia_en_medicine-app_maxi': 'A/Wikipedia:WikiProject_Medicine/Open_Textbook_of_Medicine2',
