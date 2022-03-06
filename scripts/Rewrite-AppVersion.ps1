e6059288193c267bb1d746a558d826a314ca8aba
Jaifroid <egk10@cam.ac.uk>
2022-03-06 09:23:01 +0000

    Fix rewriting of app version in Windows
    Former-commit-id: 413e24fb0d911ae26f9e0a976bafc4c9fa913eb9 [formerly fe24164cf6618929788b173acea475a4062f4d9a] [formerly 0dac558306f0a53daae82e62da2885787a35126a [formerly f9b134a4c9b58b754f43ee197cf14af0352d3928]]
    Former-commit-id: 28d8d01761d44f76508fa44465dead84b5ead02d [formerly bcf64dae7d7403bf2b182b312ab80f01d2f074a5]
    Former-commit-id: 4fd0fd850c1a618004479d77b90e1b1dcf8c7151

---
+++ scripts/Rewrite-AppVersion.ps1
@@ -0,0 +1,17 @@
+﻿# Sets the App Version Number if $INPUT_VERSION is provided
+# Script is intended to be run by a GitHub Action
+if ($INPUT_VERSION) {
+    $VERSION = $INPUT_VERSION
+} else {
+    $VERSION = $TAG_VERSION
+}
+if ($VERSION -match '^v?[\d.]') {
+    $VERSION = $VERSION -replace '^v', ''
+    "Setting App Version to $VERSION in service-worker.js and init.js ..."
+    (Get-Content ./service-worker.js) -replace '(appVersion\s*=\s*["''])[^"'']+', "`${1}$VERSION" | Set-Content -encoding "utf8BOM" ./service-worker.js
+    (Get-Content ./www/js/init.js) -replace '(appVersion..\s*=\s*["''])[^"'']+', "`${1}$VERSION" | Set-Content -encoding "utf8BOM" ./www/js/init.js
+    "Setting App Version to $VERSION-E in package.json ...`n"
+    (Get-Content ./package.json) -replace '(version[''"]:\s*["''])[^"'']+', "`${1}$VERSION-E" | Set-Content ./package.json
+} else {
+    "No valid INPUT_VERSION or TAG_VERSION were provided. File version numbers were unchanged.`n"
+}
