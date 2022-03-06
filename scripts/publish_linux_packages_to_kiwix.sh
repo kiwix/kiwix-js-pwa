8af81625deb191e21faba01edc76c4bd4cbec65f
Jaifroid <egk10@cam.ac.uk>
2022-03-06 23:28:18 +0000

    Update bash scripts
    Former-commit-id: 8d52eddf4e72893b00be5a14a2b569f251ac6dc3 [formerly 2c5adacd345cfe6fe86198867605775c9df51b90] [formerly 05c61d02ee4d888e0aa0b34b6a0ff9047ddff2a3 [formerly ab0650377fe49a5f94c89448e7eb4244e79c8ae6]]
    Former-commit-id: 432955a35d482c9ddcbcbbea008bc5185cdc58e7 [formerly 2f418171af44391b41cff2106d47309a6f91ff58]
    Former-commit-id: 24c437d62fbd2d92f688d0119ea7a79c6dbac354

--- scripts/publish_linux_packages_to_kiwix.sh
+++ scripts/publish_linux_packages_to_kiwix.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
 # Script to upload Linux packages to download.kiwix.org
 target="/data/download/release/kiwix-js-electron"
-echo "Uploading the files to https://download.kiwix.org/$target/"
-echo "ssh -o StrictHostKeyChecking=no -i ./scripts/ssh_key ci@download.kiwix.org mkdir -p $target"
+if [[ "qq${CRON_LAUNCHED}" != "qq" ]]; then
+    echo "This script was launched by the GitHub Cron proces"
+    CURRENT_DATE=$(date +'%Y-%m-%d')
+    target="/data/download/nightly/$CURRENT_DATE"
+fi
+echo "Uploading packages to https://download.kiwix.org/$target/"
+ssh -o StrictHostKeyChecking=no -i ./scripts/ssh_key ci@download.kiwix.org mkdir -p $target
 for file in ./bld/Electron/* ; do
     if [[ $file =~ \.(AppImage|deb|rpm)$ ]]; then
-        echo "scp -r -p -o StrictHostKeyChecking=no -i $file ci@download.kiwix.org:$target"
+        scp -o StrictHostKeyChecking=no -i $file ci@download.kiwix.org:$target
         echo "Copied $file to $target"
     fi
 done
