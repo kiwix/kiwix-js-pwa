49a01d3e9387cc6eb3a514953c31b49c91a58bc1
Jaifroid <egk10@cam.ac.uk>
2022-03-07 00:55:16 +0000

    Candidate publishing scripts
    Former-commit-id: 8be9bf6bc534d63299610aa8c10f4669c2b42825 [formerly 0f2fce61ae3e0782abdfa621dae68e9ba82c5e16 [formerly 30a7568c332abaa45314d63c8213778b8783b9b9]]
    Former-commit-id: 449b67a0d9dbba6a76c9c65a186efb62db853215
    Former-commit-id: 75f8fbe419e4d8b43dbc207c6c90b8f60e948d1b

---
+++ scripts/Publish-ElectronPackages.ps1
@@ -0,0 +1,19 @@
+﻿# Publish Kiwix Electron packages to Kiwix download server
+
+$target = "/data/download/release/kiwix-js-electron"
+
+if ($CRON_LAUNCHED) {
+    "`nThis script was launched by the Github Cron prccess"
+    $current_date = $(Get-Date -Format "yyyy-MM-dd")
+    $target = "/data/download/nightly/$current_date"
+}
+
+"`nUploading packages to https://download.kiwix.org$target/ ...`n"
+& "C:\Program Files\Git\usr\bin\ssh.exe" @('-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", 'ci@download.kiwix.org', "mkdir -p $target")
+ls bld/Electron/*.* | % {
+    if ($_ -match '\.(AppImage|deb|rpm)$') {
+        & "C:\Program Files\Git\usr\bin\scp.exe" @('-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", "$_", "ci@download.kiwix.org:$target")
+        "Copied $_ to $target"
+    }
+}
+""
