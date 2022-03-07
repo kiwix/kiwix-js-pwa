1c26f6b600a953d0262fef9390fec3229f2b0b07
Jaifroid <egk10@cam.ac.uk>
2022-03-07 00:28:24 +0000

    Fix scripts for publishing
    Former-commit-id: 53d217b51a5f575656f4c924b55d006e5601bf29 [formerly 82046072d2e3c87a8990dac79f3da9a1d1720bb3 [formerly 7f497a46b02d2bed12ba24a17b8b55e416b4c24f]]
    Former-commit-id: 9bd74ccb51a92eb1121d23a9729cfa10a3757912
    Former-commit-id: f32ba4d1055a9707cc2e61142661004b570e6390

--- scripts/Rewrite-AppVersion.ps1
+++ scripts/Rewrite-AppVersion.ps1
@@ -1,13 +1,20 @@
-﻿# Sets the App Version Number if $INPUT_VERSION is provided
+﻿# Sets the App Version Number if $INPUT_VERSION is provided, or sets up a nightly version if launched by CRON
 # Script is intended to be run by a GitHub Action
 if ($INPUT_VERSION) {
     $VERSION = $INPUT_VERSION
-} else {
+} elseif ($TAG_VERSION) {
     $VERSION = $TAG_VERSION
+} elseif ($CRON_LAUNCHED) {
+    $app_params = Select-String 'appVersion' "$PSScriptRoot\..\www\js\init.js" -List
+    if ($app_params -match 'params\[[''"]appVersion[''"]]\s*=\s*[''"]([^''"]+)') {
+        $app_tag = $matches[1]
+        $COMMIT_ID = $(git rev-parse --short HEAD)
+        $VERSION = "v$app_tag-$COMMIT_ID"   
+    }
 }
 if ($VERSION -match '^v?[\d.]') {
     $VERSION = $VERSION -replace '^v', ''
-    "Setting App Version to $VERSION in service-worker.js and init.js ..."
+    "`nSetting App Version to $VERSION in service-worker.js and init.js ..."
     (Get-Content ./service-worker.js) -replace '(appVersion\s*=\s*["''])[^"'']+', "`${1}$VERSION" | Set-Content -encoding "utf8BOM" ./service-worker.js
     (Get-Content ./www/js/init.js) -replace '(appVersion..\s*=\s*["''])[^"'']+', "`${1}$VERSION" | Set-Content -encoding "utf8BOM" ./www/js/init.js
     "Setting App Version to $VERSION-E in package.json ...`n"
