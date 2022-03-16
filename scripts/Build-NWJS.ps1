e24e89dc8ccbcea3bc389272f975eeecd4fc0694
Jaifroid <egk10@cam.ac.uk>
2022-03-16 21:15:43 +0000

    Update Build-NWJS.ps1
    Former-commit-id: fa6283696d4df725f935a5211cd7012e642a5dd0

--- scripts/Build-NWJS.ps1
+++ scripts/Build-NWJS.ps1
@@ -11,7 +11,7 @@ if (-Not $only32bit) {
 }
 $version10 = "0.61.0" # <<< value updated automatically from package.json if launched from Create-DraftRelease
 $versionXP = "0.14.7"
-$appBuild = "1.9.3-N" # <<< value updated auotmatically from package.json if launched from Create-DraftRelease
+$appBuild = "1.9.4-N" # <<< value updated auotmatically from package.json if launched from Create-DraftRelease
 # $ZIMbase = "wikipedia_en_100"
 # Check that the dev has included the correct archive in this branch
 $init_params = Get-Content -Raw "$PSScriptRoot\..\www\js\init.js"
