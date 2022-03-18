97e03bb67296fe3c2e1d2d8351501c03b56e9d77
Jaifroid <egk10@cam.ac.uk>
2022-03-18 07:26:14 +0000

    Create Set-AppVersion.ps1
    Former-commit-id: 4ae5f37d524831ec6da5f0d9d214d930d40c2dae
    Former-commit-id: 27021457bf81109df240e139be7aeaa269f03aeb
    Former-commit-id: 93fbb4badda6fd93adb47c2e8610a3b55de5085b
    Former-commit-id: 56b0fc8c25b5d51c6d0a9993d71fb30a8a148719

---
+++ scripts/Set-AppVersion.ps1
@@ -0,0 +1,46 @@
+﻿# Updates the app version in all required places according to the custom value
+
+[CmdletBinding()]
+param (
+    [string]$customversion = ''
+)
+
+if ($customversion) {
+    "`nUser set custom input version: $customversion"
+    $INPUT_VERSION = $customversion
+}
+
+if ($INPUT_VERSION) {
+    $VERSION = $INPUT_VERSION
+} elseif ($TAG_VERSION) {
+    $VERSION = $TAG_VERSION
+}
+
+if ($VERSION -match '^v?[\d.]') {
+    $VERSION = $VERSION -replace '^v', ''
+    "`nSetting App Version to $VERSION in service-worker.js and init.js ..."
+    (Get-Content ./service-worker.js) -replace '(appVersion\s*=\s*["''])[^"'']+', "`${1}$VERSION" | Set-Content -encoding "utf8BOM" ./service-worker.js
+    (Get-Content ./www/js/init.js) -replace '(appVersion..\s*=\s*["''])[^"'']+', "`${1}$VERSION" | Set-Content -encoding "utf8BOM" ./www/js/init.js
+    $PackageJson = Get-Content -Raw ./package.json
+    $PackageJsonNW = Get-Content -Raw ./package.json.nwjs
+    $nwVersion = $PackageJsonNW -match '"build":\s\{[^"]+"nwVersion":\s"([^"'']+)'
+    $nwVersion = $matches[1]
+    $CustomVersion = $VERSION -replace '^([^-]+)', '$1-E'
+    "Setting App Version to $CustomVersion in package.json ...`n"
+    $PackageJson = $PackageJson -replace '("version":\s+")[^"]+', "`${1}$CustomVersion"
+    Set-Content -encoding "utf8BOM" ./package.json $PackageJson
+    if ($nwVersion) {
+        $CustomVersion = $CustomVersion -creplace '-E', '-N'
+        "Setting package.json.nwjs to $CustomVersion ..."
+        $PackageJsonNW = $PackageJsonNW -replace '("version":\s+")[^"]+', "`${1}$CustomVersion"
+        Set-Content -encoding "utf8BOM" ./package.json.nwjs $PackageJsonNW
+        $BuildNWJSScript = Get-Content -Raw ./scripts/Build-NWJS.ps1
+        "Setting App Version to $CustomVersion in Build-NWJS.ps1 ..."
+        $BuildNWJSScript = $BuildNWJSScript -replace '(appBuild\s*=\s*["''])[^"'']+', ("`${1}$CustomVersion")
+        "Setting NWJS build to $nwVersion in Build-NWJS.ps1 ..."
+        $BuildNWJSScript = $BuildNWJSScript -replace '(version10\s*=\s*["''])[^"'']+', "`${1}$nwVersion"
+        Set-Content -encoding "utf8BOM" ./scripts/Build-NWJS.ps1 $BuildNWJSScript
+    }
+} else {
+    "No valid INPUT_VERSION or TAG_VERSION were provided. File version numbers were unchanged.`n"
+}
