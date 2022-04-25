86344e2f0b85c54f7af797613f0d63ae0836369e
Jaifroid <egk10@cam.ac.uk>
2022-04-25 01:27:36 +0100

    Enable limited support for Zimit ZIMs #249 (#248)
    Former-commit-id: fb65802f6075bfd2de2f00c636b1c9f59b07a51f [formerly 9d6091c79c2b24e714cb5214284aa1ebf2b0b0fb] [formerly db8e5eec98ea6333c6d34d6dd7e0c5c112a217aa [formerly 9b2a543c6c882c29e6889169fea914caa3136a32]]
    Former-commit-id: a65562bd36582a4098179544d7f21aa8b80f6b18 [formerly 16371c7c46feca2993cac90d9c4a75ffb6bb264b]
    Former-commit-id: 556babe8667fedfa263936450321e787b8cb522d

--- www/js/lib/uiUtil.js
+++ www/js/lib/uiUtil.js
@@ -324,14 +324,14 @@ define(rqDef, function(util) {
     /**
      * Displays a Bootstrap warning alert with information about how to access content in a ZIM with unsupported active UI
      */
-    function displayActiveContentWarning() {
+    function displayActiveContentWarning(type) {
         // We have to add the alert box in code, because Bootstrap removes it completely from the DOM when the user dismisses it
         var alertHTML =
             '<div id="activeContent" class="alert alert-warning alert-dismissible fade in" style="margin-bottom: 0;">' +
                 '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
                 '<strong>Unable to display active content:</strong> To use Archive Index <b><i>type a space</i></b> in the box above, or else ' +
                 '<a id="swModeLink" href="#contentInjectionModeDiv" class="alert-link">switch to Service Worker mode</a> ' +
-                'if your platform supports it. &nbsp;[<a id="stop" href="#otherSettingsDiv" class="alert-link">Permanently hide</a>]' +
+                'if your platform supports it. &nbsp;[<a id="stop" href="#expertSettingsDiv" class="alert-link">Permanently hide</a>]' +
             '</div>';
         if (params.contentInjectionMode === 'serviceworker' && (params.manipulateImages || params.displayHiddenBlockElements || params.allowHTMLExtraction)) {
             alertHTML =
@@ -342,7 +342,21 @@ define(rqDef, function(util) {
                 params.manipulateImages ? '<a id="imModeLink" href="#imageManipulationDiv" class="alert-link">disable Image manipulation</a> ' : '') + 
                 (params.allowHTMLExtraction ? (params.displayHiddenBlockElements || params.manipulateImages ? 'and ' : '') + 
                 'disable Breakout link ' : '') + 'for this content to work properly. To use Archive Index <b><i>type a space</i></b> ' +
-                'in the box above.&nbsp;[<a id="stop" href="#otherSettingsDiv" class="alert-link">Permanently hide</a>]' +
+                'in the box above.&nbsp;[<a id="stop" href="#expertSettingsDiv" class="alert-link">Permanently hide</a>]' +
+            '</div>';
+        }
+        if (type === 'zimit') {
+            alertHTML =
+            '<div id="activeContent" class="alert alert-warning alert-dismissible fade in" style="margin-bottom: 0;">' +
+                '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
+                '<strong>' + (params.contentInjectionMode === 'jquery' ? 'Limited Zimit' : 'Experimental') + ' support:</strong> ' + 
+                (params.contentInjectionMode === 'jquery' ? 'Please <a id="swModeLink" href="#contentInjectionModeDiv" ' + 
+                'class="alert-link">switch to Service Worker mode</a> if your platform supports it. ' : 
+                'Please note that this app only has experimental support for <b>Zimit</b> ZIMs, and ' + 
+                'in particular it may not run all active content. Audio and video may not work yet. ') + 
+                '<br />Search for your content in the search box above (start your search with .* to match part of a title)' +
+                ', or to use the Archive Index <b><i>type a space</i></b> in the box above.&nbsp;' +
+                '[<a id="stop" href="#expertSettingsDiv" class="alert-link">Permanently hide</a>]' +
             '</div>';
         }
         var alertBoxHeader = document.getElementById('alertBoxHeader');
