3369d8cc243bed8d2ab7de6c694b18ae085c95f6
Jaifroid <egk10@cam.ac.uk>
2022-05-01 23:34:52 +0100

    Revert test
    Former-commit-id: 2be3242c9238efc738fb3042a4dab7176776a100 [formerly b12ae417d82c2330e461a97bb51f0bc73337ffde] [formerly a18263276ea2958f7257d8b9c6d7961f051c008d] [formerly 22a16646142b9aac842e7f7efdc465e462e44329 [formerly 21e05a19b27e73b14f5d9f7a5b93d71349726387 [formerly 577537b73aaf84b8fc49449d37ac31da5d87966e]]]
    Former-commit-id: 26d52d4ccfe1fd10f4a51cba17171471288e59c0 [formerly 85069c5442b3a18b06daf1e30adaad0c3b41265c [formerly 33fc70aa14905b545d7fd24cda32b8b0cee83934]]
    Former-commit-id: 78250cb96335b4793a8d3c57fedfd89b2968e303 [formerly 2ff209b8ab77874a9abaa7b593541f9ccadf2e83]
    Former-commit-id: 475e2cde0cfbfd57cb98e491f4a890621f65ea44

--- www/js/app.js
+++ www/js/app.js
@@ -3811,7 +3811,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'cache', 'images', 'sett
                                 if (content.buffer) {
                                     // In Edge Legacy, we have to transfer the buffer inside an array, whereas in Chromium, this produces an error
                                     // due to type not being transferrable... (and already detached, which may be to do with storing in IndexedDB in Electron)
-                                    // if (/UWP/.test(params.appType)) buffer = [buffer];
+                                    if (/UWP/.test(params.appType)) buffer = [buffer];
                                     messagePort.postMessage(message, buffer);
                                 } else {
                                     messagePort.postMessage(message);
