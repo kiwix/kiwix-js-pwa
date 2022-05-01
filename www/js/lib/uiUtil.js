9d8bf8dbda131cd28a21388f638d282041318826
Jaifroid <egk10@cam.ac.uk>
2022-05-01 09:44:47 +0100

    Shorten support message

--- www/js/lib/uiUtil.js
+++ www/js/lib/uiUtil.js
@@ -352,10 +352,9 @@ define(rqDef, function(util) {
                 '<strong>' + (params.contentInjectionMode === 'jquery' ? 'Limited Zimit' : 'Experimental') + ' support:</strong> ' + 
                 (params.contentInjectionMode === 'jquery' ? 'Please <a id="swModeLink" href="#contentInjectionModeDiv" ' + 
                 'class="alert-link">switch to Service Worker mode</a> if your platform supports it. ' : 
-                'Please note that this app only has experimental support for <b>Zimit</b> ZIMs, and ' + 
-                'in particular it may not run all active content. Audio and video may not work yet. ') + 
-                '<br />Search for your content in the search box above (start your search with .* to match part of a title)' +
-                ', or to use the Archive Index <b><i>type a space</i></b> in the box above.&nbsp;' +
+                'This app only has experimental support for <b>Zimit</b> ZIMs. It may not run all active content or play audio/video. ') + 
+                '<br />Search for your content above. Start your search with <b>.*</b> to match part of a title. ' +
+                'To use the Archive Index <b><i>type a space</i></b> in box above.&nbsp;' +
                 '[<a id="stop" href="#expertSettingsDiv" class="alert-link">Permanently hide</a>]' +
             '</div>';
         }
