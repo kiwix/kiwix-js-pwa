60fcaa54941268550e8e46ffc2bf7d642cbcede3
Jaifroid <egk10@cam.ac.uk>
2022-06-05 15:48:00 +0100

    Cannot determine UWP type in Windows Mobile

--- www/js/app.js
+++ www/js/app.js
@@ -874,7 +874,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'utf8', 'cache', 'images
             }
             // If it's a PWA that is not also an Electron/NWJS or UWP app, don't check for updates
             if (/^(?!.*(Electron|UWP)).*PWA/.test(params.appType)) return;
-            if (/UWP/.test(params.appType && Windows.ApplicationModel && Windows.ApplicationModel.AppInfo &&
+            if (/UWP/.test(params.appType && Windows.ApplicationModel && Windows.ApplicationModel.AppInfo.current &&
                 !/Association.Kiwix/.test(Windows.ApplicationModel.AppInfo.current.package.id.publisher)))
                 return; // It's a UWP app installed from the Store, so it will self update
             // GitHub updates
@@ -1872,7 +1872,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'util', 'utf8', 'cache', 'images
                 // On some platforms, bootstrap's jQuery functions have not been injected yet, so we have to run in a timeout
                 setTimeout(function () {
                     $('#myModal').on('hide.bs.modal', function () {
-                        if (/UWP/.test(params.appType && Windows.ApplicationModel && Windows.ApplicationModel.AppInfo &&
+                        if (/UWP/.test(params.appType && Windows.ApplicationModel && Windows.ApplicationModel.AppInfo.current &&
                             !/Association.Kiwix/.test(Windows.ApplicationModel.AppInfo.current.package.id.publisher)))
                             return; // It's a UWP app installed from the Store, so it will self update
                         if (!params.allowInternetAccess) {
