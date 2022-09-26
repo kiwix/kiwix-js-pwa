42583433157133081163a23b85bfef05026efc8a
Jaifroid <egk10@cam.ac.uk>
2022-09-26 23:02:35 +0100

    Bump NWJS version
    Former-commit-id: bcba7db5fd3c71dbf8de251a78e7f2aed7ef0edf
    Former-commit-id: 639bfa546a1b55fa9c75b06684208662f1671127

--- scripts/Build-NWJS.ps1
+++ scripts/Build-NWJS.ps1
@@ -9,7 +9,7 @@ if (-Not $only32bit) {
 } else {
     "Caller requested only32bit build"
 }
-$version10 = "0.65.0" # <<< value updated automatically from package.json if launched from Create-DraftRelease
+$version10 = "0.68.1" # <<< value updated automatically from package.json if launched from Create-DraftRelease
 $versionXP = "0.14.7"
 $appBuild = "2.1.8-N" # <<< value updated auotmatically from package.json if launched from Create-DraftRelease
 # $ZIMbase = "wikipedia_en_100"
