030dfe0047be37ce6d94dea4be96e4f8c70018fb
Jaifroid <egk10@cam.ac.uk>
2023-05-03 08:21:50 +0100

    Make a dist filder and copy app

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,18 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import copy from 'rollup-plugin-copy';
 
 const config = {
     input: 'www/js/app.js',
     output: {
-        file: 'www/js/bundle.js',
+        file: 'dist/www/js/bundle.js',
         format: 'iife',
         name: 'KiwixJSBundle'
     },
     plugins: [
         babel({ 
-            exclude: "node_modules/**",
+            exclude: 'node_modules/**',
             babelHelpers: 'bundled' 
         }),
         resolve({
@@ -23,6 +24,19 @@ const config = {
         replace({
             // This is needed to prevent a fatal error in IE11 (bug with the URL constructor polyfill)
             'document.baseURI' : "document.location.href.replace(/[^/]*$/, '')"
+        }),
+        copy({
+            targets: [
+              { src: ['www/**', '!www/js/app.js', '!www/js/lib'], dest: 'dist/www', expandDirectories: true, onlyFiles: true },
+              { src: ['service-worker.js', 'index.html', 'manifest.json'], dest: 'dist' },
+            ],
+            flatten: false
+        }),
+        copy({
+            targets: [
+              { src: 'www/js/lib/*dec-wasm.wasm', dest: 'dist/www/js' }
+            ],
+            flatten: true
         })
     ]
 };
