db711e8e8fb5d2f3267a22d883929036f4086660
Jaifroid <egk10@cam.ac.uk>
2023-06-26 21:46:15 +0100

    Restored required functions accidentally deleted

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,16 @@ module.exports = {
     rules: {
         semi: 0,
         indent: ['error', 4],
-        'dot-notation': 0
+        'dot-notation': 0,
+        'no-var': 0,
+        'no-mixed-operators': 0,
+        'no-extra-parens': 1,
+        'no-unused-expressions': 1,
+        'no-unused-vars': 1,
+        'n/no-callback-literal': 0,
+        'object-shorthand': 0,
+        'multiline-ternary': 0,
+        'no-extend-native': 0,
+        'no-global-assign': 0
     }
 }
