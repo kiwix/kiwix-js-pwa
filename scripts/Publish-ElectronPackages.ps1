16e79c2be29b9d1b1a105a2f303f67db069ab158
Jaifroid <egk10@cam.ac.uk>
2023-07-23 18:59:03 +0300

    Make sure it is not run with nightly

--- scripts/Publish-ElectronPackages.ps1
+++ scripts/Publish-ElectronPackages.ps1
@@ -179,7 +179,7 @@ if (-not $githubonly) {
                 $renamed_file = $renamed_file -replace '^.*?([\\/]bld)', './dist$1' -replace '[\\/]', '/'
                 "Copying $renamed_file to $target..."
                 & "C:\Program Files\Git\usr\bin\scp.exe" @('-P', '30022', '-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", "$renamed_file", "ci@master.download.kiwix.org:$target")
-                if ($renamed_file -match '\.appx$') {
+                if (!$CRON_LAUNCHED -and $renamed_file -match '\.appx$') {
                     "Also copying $renamed_file to $win_target..."
                     $renamed_win_file = $renamed_file -replace 'electron', 'windows'
                     mv $renamed_file $renamed_win_file
