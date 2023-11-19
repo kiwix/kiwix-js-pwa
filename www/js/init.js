d49358e3da64d2769a95a1dd64f0517f0f085fad
Jaifroid <egk10@cam.ac.uk>
2023-11-19 07:20:40 +0000

    Fix formatting

--- www/js/init.js
+++ www/js/init.js
@@ -139,7 +139,7 @@ params['lockDisplayOrientation'] = getSetting('lockDisplayOrientation'); // 'por
 params['noHiddenElementsWarning'] = getSetting('noHiddenElementsWarning') !== null ? getSetting('noHiddenElementsWarning') : false; // A one-time warning about Hidden elements display
 
 // Apply any override parameters in querystring (done as a self-calling function to avoid creating global variables)
-(function overrideParams() {
+(function overrideParams () {
     var rgx = /[?&]([^=]+)=([^&]+)/g;
     var matches = rgx.exec(window.location.search);
     while (matches) {
@@ -285,7 +285,7 @@ if (/^http/i.test(window.location.protocol)) {
 }
 
 // Get app type
-function getAppType() {
+function getAppType () {
     var type = 'HTML5';
     if (typeof Windows !== 'undefined' && typeof Windows.Storage !== 'undefined') type = 'UWP';
     if (window.fs || window.nw) type = 'Electron';
@@ -299,7 +299,7 @@ function getAppType() {
 
 // Set up storage types
 // First check that we have not simply upgraded the app and the packaged file
-params.packagedFileStub = params.packagedFile ? params.packagedFile.replace(/(?:-app_maxi)?_[\d-]+\.zim\w?\w?$/, ''): null;
+params.packagedFileStub = params.packagedFile ? params.packagedFile.replace(/(?:-app_maxi)?_[\d-]+\.zim\w?\w?$/, '') : null;
 if (params.packagedFileStub && params.appVersion !== getSetting('appVersion') && ~params.storedFile.indexOf(params.packagedFileStub)) {
     console.log('The packaged archive has been upgraded: resetting file pointers to point to ' + params.packagedFile);
     params.lastPageVisit = '';
@@ -357,7 +357,7 @@ var divInstall2 = document.getElementById('divInstall2');
 var btnInstall2 = document.getElementById('btnInstall2');
 var btnLater = document.getElementById('btnLater');
 
-window.addEventListener('beforeinstallprompt', function(e) {
+window.addEventListener('beforeinstallprompt', function (e) {
     console.debug('beforeinstallprompt fired');
     // Prevent Chrome 76 and earlier from automatically showing a prompt
     e.preventDefault();
@@ -373,7 +373,7 @@ window.addEventListener('beforeinstallprompt', function(e) {
         btnLater.addEventListener('click', function (e) {
             e.preventDefault();
             divInstall1.innerHTML = '<b>You can install this app later from Configuration</b>';
-            setTimeout(function() {
+            setTimeout(function () {
                 divInstall1.style.display = 'none';
             }, 4000);
             params.installLater = true;
@@ -383,14 +383,14 @@ window.addEventListener('beforeinstallprompt', function(e) {
     deleteSetting('PWAInstalled');
 });
 
-function installApp(e) {
+function installApp (e) {
     e.preventDefault();
     // Show the prompt
     deferredPrompt.prompt();
     btnInstall1.disabled = true;
     btnInstall2.disabled = true;
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then(function(choiceResult) {
+    deferredPrompt.userChoice.then(function (choiceResult) {
         if (choiceResult.outcome === 'accepted') {
             console.log('PWA installation accepted');
             divInstall1.style.display = 'none';
@@ -405,12 +405,12 @@ function installApp(e) {
     });
 }
 
-window.addEventListener('appinstalled', function(e) {
+window.addEventListener('appinstalled', function (e) {
     params.PWAInstalled = params.appVersion;
     setSetting('PWAInstalled', params.PWAInstalled);
 });
 
-function getSetting(name) {
+function getSetting (name) {
     var result;
     if (params.storeType === 'cookie') {
         var regexp = new RegExp('(?:^|;)\\s*' + name + '=([^;]+)(?:;|$)');
@@ -423,7 +423,7 @@ function getSetting(name) {
     return result === null || result === 'undefined' ? null : result === 'true' ? true : result === 'false' ? false : result;
 }
 
-function setSetting(name, val) {
+function setSetting (name, val) {
     if (params.storeType === 'cookie') {
         document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(val) + ';expires=Fri, 31 Dec 9999 23:59:59 GMT';
     }
@@ -435,7 +435,7 @@ function setSetting(name, val) {
 }
 
 // NB This only deals with simple names that don't need to be URI-encoded
-function deleteSetting(name) {
+function deleteSetting (name) {
     if (params.storeType === 'cookie') {
         document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Strict';
     } else if (params.storeType === 'local_storage') {
@@ -447,7 +447,7 @@ function deleteSetting(name) {
 // DEV: This function is replicated from settingsStore.js because it's not available from init
 // It returns 'cookie' if the always-present contentInjectionMode is still in cookie, which
 // means the store previously used cookies and hasn't upgraded yet: this won't be done till app.js is loaded
-function getBestAvailableStorageAPI() {
+function getBestAvailableStorageAPI () {
     var type = 'none';
     var localStorageTest;
     try {
