b842c9ae137db5d7079172a547838fcc75ac3549
Jaifroid <egk10@cam.ac.uk>
2023-11-19 16:10:56 +0000

    Bump app version to evict cache

--- service-worker.js
+++ service-worker.js
@@ -31,7 +31,7 @@
  * download and install a new copy; we have to hard code this here because it is needed before any other file
  * is cached in APP_CACHE
  */
-const appVersion = '2.7.82';
+const appVersion = '2.7.83';
 
 /**
  * The name of the Cache API cache in which assets defined in regexpCachedContentTypes will be stored
