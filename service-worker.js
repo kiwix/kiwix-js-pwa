8347b04f88d069207b401ad83f6b2fecfc62992e
Jaifroid <egk10@cam.ac.uk>
2023-12-11 16:59:17 +0000

    Fix path when searching for darkreader

--- service-worker.js
+++ service-worker.js
@@ -351,7 +351,7 @@ self.addEventListener('fetch', function (event) {
     // Filter out requests that do not match the scope of the Service Worker
     if (/\/dist\/(www|[^/]+?\.zim)\//.test(rqUrl) && !/\/dist\//.test(self.registration.scope)) return;
     // Filter darkReader request transformed by wombat.js
-    if (/\.zim.*\/www\/js\/lib\/darkreader\.min\.js/.test(rqUrl)) {
+    if (/\.zim.*\/www\/js\/(?:lib\/)?darkreader\.min\.js/.test(rqUrl)) {
         rqUrl = rqUrl.replace(/^([^:]+:\/\/[^/]+)(?:[^/]|\/(?!js_\/))+\/js_\/[^:]+:\/\/[^/]+(.+)/, '$1$2');
     }
     var urlObject = new URL(rqUrl);
