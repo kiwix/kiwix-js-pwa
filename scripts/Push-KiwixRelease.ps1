821cb4a623b31da350e423b10e2fc5ca501fb5b8
Jaifroid <egk10@cam.ac.uk>
2024-02-01 22:07:42 +0000

    Update Push-KiwixRelease.ps1
    Fixes #531

--- scripts/Push-KiwixRelease.ps1
+++ scripts/Push-KiwixRelease.ps1
@@ -1,5 +1,5 @@
 ﻿param (
-    [string]$filename = "",
+    [array]$filenames = @(),
     [switch]$tag = $false,
     [switch]$dryrun = $false,
     [switch]$yes = $false,
@@ -9,23 +9,24 @@
 function Main {
 
     # Deal with cases where no directory or filename is entered
-    if (($filename -eq "") -and (!$help)) { 
+    if (($filenames.count -lt 1) -and (!$help)) { 
         $filename = Read-Host "Enter the filename to upload to download.kiwix.org/releases/ or ? for help"
         if ($filename -eq "") {
             exit
         }
+        $filenames = @($filename)
         ' '
     }
 
     # Check whether user asked for help
-    if (($filename -eq "?") -or ($help)) {
+    if (($filenames -eq "?") -or ($help)) {
         Get-PushHelp
         exit
     }
 
     # Download the file from GitHub if a tag was entered
     if ($tag) {
-        $tagname = $filename
+        $tagname = $filenames
         # Check if a Github Token is available
         if (-Not $GITHUB_TOKEN) {
             $GITHUB_TOKEN = Get-Content -Raw "$PSScriptRoot/github_token"
@@ -68,96 +69,100 @@ function Main {
             Write-Warning "No release matching $tag_name was found on the server!"
             exit 1
         }
-        $asset_found = $false
-        $asset = $null
         $assets = $release.assets
-        $assets | Where-Object { $asset_found -eq $false } | % {
-            $asset = $_
-            if ($asset.name -imatch '\.appxbundle$') {
-                $asset_found = $true
+        $assets_found = @()
+        $assets | % {
+            if ($_.name -imatch '\.appx(bundle)?$') {
+                $assets_found += $_
             }
         }
-        if ($asset_found) {
+        # Test if one or more assets were found
+
+        if ($assets_found.count -ge 1) {
             if ($dryrun) {
-                $asset_json = $asset | ConvertTo-Json
-                "`n[DRYRUN:] Asset found: `n$asset_json"
+                $count = $assets_found.count
+                $assets_json = $assets_found | ConvertTo-Json
+                "`n[DRYRUN:] $count asset(s) found: `n$assets_json"
             }
         } else {
             Write-Warning "No asset matching an appxbundle was found on the server!"
             exit 1
         }
-        if ($asset.browser_download_url) {
-            $download_url = $asset.browser_download_url 
-            "`nDownloading $download_url"
-            if (!$dryrun) {
-                Invoke-WebRequest $download_url -OutFile $asset.name
+        foreach ($asset in $assets_found) {
+            if ($asset.browser_download_url) {
+                $download_url = $asset.browser_download_url 
+                "`nDownloading $download_url"
+                if (!$dryrun) {
+                    Invoke-WebRequest $download_url -OutFile $asset.name
+                }
+            } else {
+                Write-Warning "Could not get the download URL for the asset!"
+                "$asset"
+                exit 1
+            }
+        }
+
+        # We should finally have the files!        
+        $filenames = dir "*$numeric_tagname*.appx*" | Select-Object -ExpandProperty Name
+        if ($filenames -and $filenames.count -gt 0) {
+            foreach ($filename in $filenames) {
+                "Setting file to $filename..."
             }
         } else {
-            Write-Warning "Could not get the download URL for the asset!"
-            "$asset_json"
-            exit 1
-        }
-        # We should finally have the file!
-        # $filename = dir "$PSScriptRoot/../dist/AppPackages/*_$tagname*_Test/*_$tagname*.appx*"
-        $filename = dir "*_$numeric_tagname*.appx*"
-        if ($filename -and $filename.count -eq 1) {
-            "Setting file to $filename..."
-        } elseif ($filename.count -ge 2) {
-            "More than one file matches that tag!"
-            exit 1
-        } else {
-            "No package matching that tag was found. Perhaps the download failed?"
+            "No packages matching that tag was found. Perhaps the download failed?"
             if ($dryrun) {
                 exit 1
             }
         }
     }
 
-    # If the path is a file of the right type, ask for confirmation 
-    if ((Test-Path $filename -PathType leaf) -and ($filename -imatch '(.*)\.(?:appx|appxbundle|appxupload)$')) {
-        $newfilename = $filename -ireplace '^.*\\[^\d]+([\d.]+?)\.0_[^\d]+?(\.appx(?:bundle|upload))$', 'kiwix-js-pwa_$1$2'
-        $filename = $filename -ireplace '[\\/]', '/'
-        $file = $filename -ireplace '^.*/([^/]+$)', '$1'
-        $target = '/data/download/release/kiwix-js-pwa'
-        "$filename is ready to upload to $target ..."
-        if ($dryrun) { "DRY RUN: no upload will be made" }
-        if (! $yes) {
-            $response = Read-Host "Do you wish to proceed? Y/N"
-            if ($response -ne "Y") {
-                Write-Warning "Aborting upload because user cancelled."
+    # Run code below for each filename in filenames
+    foreach ($filename in $filenames) {
+        # If the path is a file of the right type, ask for confirmation 
+        if ((Test-Path $filename -PathType leaf) -and ($filename -imatch '(.*)\.(?:appx|appxbundle|appxupload)$')) {
+            $newfilename = $filename -ireplace '^.*?([\d.]+)[^.]*(\.appx(?:bundle|upload)?)$', 'kiwix-js-windows_$1$2'
+            $filename = $filename -ireplace '[\\/]', '/'
+            $file = $filename -ireplace '^.*/([^/]+$)', '$1'
+            $target = '/data/download/release/kiwix-js-windows'
+            "$filename is ready to upload to $target ..."
+            if ($dryrun) { "DRY RUN: no upload will be made" }
+            if (! $yes) {
+                $response = Read-Host "Do you wish to proceed? Y/N"
+                if ($response -ne "Y") {
+                    Write-Warning "Aborting upload because user cancelled."
+                }
             }
-        }
-        if ($yes -or $response -eq "Y") {
-            $keyfile = "$PSScriptRoot\ssh_key"
-            $keyfile = $keyfile -ireplace '[\\/]', '/'
-            if ($dryrun) {
-                "C:\Program Files\Git\usr\bin\scp.exe -P 30022 -o StrictHostKeyChecking=no -i $keyfile $filename ci@master.download.kiwix.org:$target"
-                "echo 'rename $target/$file $target/$newfilename' | C:\Program Files\Git\usr\bin\sftp.exe -P 30022 -o StrictHostKeyChecking=no -i $keyfile ci@master.download.kiwix.org"
-            } else {
-                # Uploading file
-                & "C:\Program Files\Git\usr\bin\scp.exe" @('-P', '30022', '-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", "$filename", "ci@master.download.kiwix.org:$target")
-                echo "rename $target/$file $target/$newfilename" | & "C:\Program Files\Git\usr\bin\sftp.exe" @('-P', '30022', '-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", 'ci@master.download.kiwix.org')
-                Write-Host "`nDone.`n" -ForegroundColor Green
+            if ($yes -or $response -eq "Y") {
+                $keyfile = "$PSScriptRoot\ssh_key"
+                $keyfile = $keyfile -ireplace '[\\/]', '/'
+                if ($dryrun) {
+                    "C:\Program Files\Git\usr\bin\scp.exe -P 30022 -o StrictHostKeyChecking=no -i $keyfile $filename ci@master.download.kiwix.org:$target"
+                    "echo 'rename $target/$file $target/$newfilename' | C:\Program Files\Git\usr\bin\sftp.exe -P 30022 -o StrictHostKeyChecking=no -i $keyfile ci@master.download.kiwix.org"
+                } else {
+                    # Uploading file
+                    & "C:\Program Files\Git\usr\bin\scp.exe" @('-P', '30022', '-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", "$filename", "ci@master.download.kiwix.org:$target")
+                    echo "rename $target/$file $target/$newfilename" | & "C:\Program Files\Git\usr\bin\sftp.exe" @('-P', '30022', '-o', 'StrictHostKeyChecking=no', '-i', "$keyfile", 'ci@master.download.kiwix.org')
+                    Write-Host "`nDone.`n" -ForegroundColor Green
+                }
             }
-        }
-        $response = $null
-        if (-Not $yes -and $asset_found) {
-            if ($dryrun) {
-                "[DRYRUN:] No file will be deleted, you can answer this question as you wish"
+            $response = $null
+            if (-Not $yes) {
+                if ($dryrun) {
+                    "[DRYRUN:] No file will be deleted, you can answer this question as you wish"
+                }
+                ""
+                $response = Read-Host "Do you wish to delete $filename that we downloaded? Y/N"
+                if (!$dryrun -and ($response -eq "Y")) {
+                    "Deleting..."
+                    del $filename
+                } else {
+                    "[DRYRUN:] No files were deleted"
+                }
             }
-            ""
-            $response = Read-Host "Do you wish to delete $filename that we downloaded? Y/N"
-            if (!$dryrun -and ($response -eq "Y")) {
-                "Deleting..."
-                del $filename
-            } else {
-                "[DRYRUN:] No files were deleted"
-            }
+        } else {
+            "You can only upload a file of type .appx, .appxbundle or .appxupload"
         }
         Write-Host "`nAll operations successfully finished.`n" -ForegroundColor Green
-    } else {
-        "You can only upload a file of type .appx, .appxbundle or .appxupload"
-        exit
     }
 }
 function Get-PushHelp {
@@ -169,7 +174,7 @@ function Get-PushHelp {
     For download to work, ensure you have your GitHub access token in .\scripts\github_token
     To upload to the Kiwix server, ensure you have the ssh_key in .\scripts\ssh_key
     
-    FILENAME|TAG    the filename to upload (must be .appx, .appxupload, .appxbundle),
+    FILENAMES|TAG   the filename(s) to upload (must be .appx, .appxupload, .appxbundle),
         or ?        or the TAG name (if -tag is set), or ? for help
     -dryrun         tests that the file exists and is of the right type, but does not
                     upload it
