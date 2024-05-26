14918b32798c531da2f5e8fdc7968daf5d17cd3f
Jaifroid <egk10@cam.ac.uk>
2024-05-26 18:23:20 +0100

    Update again

--- www/js/app.js
+++ www/js/app.js
@@ -6833,10 +6833,6 @@ function addListenersToLink (a, href, baseUrl) {
             a.touched = false;
             a.newcontainer = false;
         }
-        if ('MSBlobBuilder' in window) {
-            a.style.webkitUserSelect = 'auto';
-            a.style.msUserSelect = 'auto';
-        }
         loadingContainer = false;
         a.articleisloading = false;
         a.dataset.touchevoked = false;
@@ -6925,10 +6921,6 @@ function addListenersToLink (a, href, baseUrl) {
             timeout = 200;
         }
         a.touched = true;
-        if (a.style.userSelect === undefined && appstate.wikimediaZimLoaded && params.showPopoverPreviews) {
-            // This prevents selection of the text in a touched link in iOS Safari
-            a.style.webkitUserSelect = 'none';
-        }
         var event = e;
         // The link will be clicked if the user long-presses for more than 500ms (if the option is enabled), or 200ms for popover
         setTimeout(function () {
@@ -7018,6 +7010,7 @@ function addListenersToLink (a, href, baseUrl) {
         // Prevent accidental selection of the anchor text in some contexts
         if ('MSBlobBuilder' in window) {
             // This prevents selection of the text in a touched link in the Edge Legacy / UWP or IE11
+            // Note that setting this for Safari on iOS prevents the popover from appearing
             a.style.webkitUserSelect = 'none';
             a.style.msUserSelect = 'none';
         }
