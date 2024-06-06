427220f5627e0d683dbc11f66f97f7129e5a00ea
Jaifroid <egk10@cam.ac.uk>
2024-06-06 07:37:05 +0100

    Revert "Fix blocked mailto in Chromium browsers"
    This reverts commit 91b8b93cd2b218286366cbab5496a8c4713bd804.

--- service-worker.js
+++ service-worker.js
@@ -704,7 +704,7 @@ function fetchUrlFromZIM (urlObjectOrString, range, expectedHeaders) {
                 var headers = new Headers();
                 if (contentLength !== null) headers.set('Content-Length', contentLength);
                 // Set Content-Security-Policy to sandbox the content (prevent XSS attacks from malicious ZIMs)
-                headers.set('Content-Security-Policy', "default-src 'self' data: file: blob: about: mailto: chrome-extension: bingmaps: https://pwa.kiwix.org https://kiwix.github.io 'unsafe-inline' 'unsafe-eval'; sandbox allow-scripts allow-same-origin allow-modals allow-popups allow-forms allow-downloads;");
+                headers.set('Content-Security-Policy', "default-src 'self' data: file: blob: about: chrome-extension: bingmaps: https://pwa.kiwix.org https://kiwix.github.io 'unsafe-inline' 'unsafe-eval'; sandbox allow-scripts allow-same-origin allow-modals allow-popups allow-forms allow-downloads;");
                 headers.set('Referrer-Policy', 'no-referrer');
                 if (contentType) headers.set('Content-Type', contentType);
 
