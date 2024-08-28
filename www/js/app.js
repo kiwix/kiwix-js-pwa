8e524f4b07c2a5fa99b9b3c27b4f425680b1ca1c
Jaifroid <egk10@cam.ac.uk>
2024-08-28 07:24:39 +0300

    Typo

--- www/js/app.js
+++ www/js/app.js
@@ -5287,7 +5287,7 @@ function filterClickEvent (event) {
             if (!/^(?:#|javascript)/i.test(decHref)) {
                 uiUtil.pollSpinner('Loading ' + decHref.replace(/([^/]+)$/, '$1').substring(0, 18) + '...');
                 // Tear down contents of previous document -- this is needed when a link in a ZIM link in an external window hasn't had
-                // an event listener attached. For example, licks in popovers in external windows. UWP doesn't allow access to the contents
+                // an event listener attached. For example, links in popovers in external windows. UWP doesn't allow access to the contents
                 // of the external window, so we can't clear it.
                 if (!/UWP/.test(params.appType) && articleWindow && articleWindow.document && articleWindow.document.body) {
                     articleWindow.document.body.innerHTML = '';
