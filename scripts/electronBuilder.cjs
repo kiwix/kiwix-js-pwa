742650db3504cb185eef360434b0e1659898ffc1
Jaifroid <egk10@cam.ac.uk>
2025-08-23 16:07:46 +0100

    Fix electron-builder skipsigning configuration

--- scripts/electronBuilder.cjs
+++ scripts/electronBuilder.cjs
@@ -15,9 +15,11 @@ const unsignedConfig = {
 };
 
 // Remove properties if they exist
+// NB Values below are deprecated but may still be present in some configs
 delete unsignedConfig.win.certificateSha1;
 delete unsignedConfig.win.signingHashAlgorithms;
 delete unsignedConfig.win.rfc3161TimeStampServer;
 delete unsignedConfig.win.sign;
-
+// This is the new key that needs to be removed
+delete unsignedConfig.win.signtoolOptions;
 module.exports = unsignedConfig;
