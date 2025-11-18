5d41fa08bbc335f382141b630acd0c6a0f6efb82
Jaifroid <egk10@cam.ac.uk>
2025-11-18 13:59:13 +0000

    Patch gitignore correctly

--- scripts/patch_gitignore.sh
+++ scripts/patch_gitignore.sh
@@ -3,10 +3,13 @@
 # Delete gitignore entry for the dist folder
 sed -i "/^\/dist\/$/d" .gitignore
 
-# Replace the /node_modules*/ entry in gitignore with the following
-sed -i 's|/node_modules\*/|/node_modules/*\
-!/node_modules/jquery\
-/node_modules/jquery/*\
-!/node_modules/jquery/dist\
-/node_modules/jquery/dist/*\
-!/node_modules/jquery/dist/jquery.slim.min.*|' .gitignore
+# Replace the /*node_modules*/ entry in gitignore with the following
+sed -i 's|/\*node_modules\*/|/node_modules/*\
+!/node_modules/bootstrap\
+/node_modules/bootstrap/*\
+!/node_modules/bootstrap/dist\
+/node_modules/bootstrap/dist/*\
+!/node_modules/bootstrap/dist/css\
+!/node_modules/bootstrap/dist/css/*\
+!/node_modules/bootstrap/dist/js\
+!/node_modules/bootstrap/dist/js/*|' .gitignore
